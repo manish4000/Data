30f79538c7c837bf59df592143d12e9d13cb82d2
manisheximgroup <manisheximgroup1@gmail.com>
2023-02-28 15:41:09 +0530

    Create multiple_select_checkbox.blade.php

---
+++ admin.gabs.biz/laravel/resources/views/components/dash/form/inputs/multiple_select_checkbox.blade.php
@@ -0,0 +1,14 @@
+@props([
+    'label',
+    'value',
+    'customClass',
+    'id'
+])
+@php 
+$customClass = (isset($customClass)) ? $customClass : '';
+@endphp
+
+<div class="custom-control  custom-checkbox custom-control-inline">
+    <input class="custom-control-input {{$customClass}}"  type="checkbox"  id="{{$id}}" value="{{$value}}">
+    <label  class="custom-control-label" for="{{$id}}"></label>
+</div>
