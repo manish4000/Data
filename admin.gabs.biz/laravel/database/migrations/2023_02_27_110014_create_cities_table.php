2d1cde90473577b19d0e35e9c296611532d8504f
manisheximgroup <manisheximgroup1@gmail.com>
2023-02-28 15:30:19 +0530

    Create 2023_02_27_110014_create_cities_table.php

---
+++ admin.gabs.biz/laravel/database/migrations/2023_02_27_110014_create_cities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('state_id');
+            $table->unsignedBigInteger('country_id');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->enum('display',['Yes','No']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
