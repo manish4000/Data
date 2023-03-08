3b129f004284ded8d6bdc1cc8fd12005e7b8f347
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-08 12:21:32 +0530

    add empty condation

--- laravel/resources/views/components/dash/form/inputs/file.blade.php
+++ laravel/resources/views/components/dash/form/inputs/file.blade.php
@@ -1,28 +1,3 @@
-{{-- @props([
-    'for',
-    'caption',
-    'name',
-    'placeholder',
-    'value'
-])
-
-<div class="media">
-    @php $logo_file = asset('images/portrait/small/avatar-s-11.jpg'); @endphp
-    @if(isset($data->logo_file) && !empty($data->logo_file))
-        @php $logo_file = 'https://www.japanesecartrade.com/logo/'.$data->logo_file;  @endphp
-    @endif
-    <a href="javascript:void(0);" class="mr-25">
-        <img src="{{$logo_file}}" id="{{ $for }}" class="rounded mr-50" alt="" height="80" width="80" />
-    </a>
-    <div class="media-body mt-75 ml-1">
-        <label for="" class="btn btn-sm btn-primary mb-75 mr-75">{{ $caption }}</label>
-        <input type="file" name="{{ $name }}" id="" hidden accept="image/*" />
-        <p>Allowed JPEG, JPG, PNG, GIF or BMP. Max size of 5MiB</p>
-    </div>
-</div> --}}
-
-
-
 @props([
     'for',
     'caption',
@@ -37,7 +12,8 @@
 @php
 $multiple = (isset($multiple))? $multiple :'';
 $imageId = (isset($imageId))? $imageId :'';
-$editImageUrl = (isset($editImageUrl))? $editImageUrl :asset('assets/images/portrait/small/avatar-s-11.jpg');
+
+$editImageUrl = (isset($editImageUrl) && !empty($editImageUrl) )? $editImageUrl :asset('assets/images/portrait/small/avatar-s-11.jpg');
 
 @endphp
 
@@ -69,7 +45,6 @@ $(document).ready(function (e) {
     let reader = new FileReader();
  
     reader.onload = (e) => { 
- 
       $('#{{$imageId}}').attr('src', e.target.result); 
     }
  
