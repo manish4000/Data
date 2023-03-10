ce6b995696905cee51fd2b43fa29aa17c99c7499
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-10 15:31:54 +0530

    update for testing

--- laravel/resources/views/content/admin/company/new_create.blade.php
+++ laravel/resources/views/content/admin/company/new_create.blade.php
@@ -95,7 +95,7 @@
                   <div class="col-md-4">
                      <div class="form-group">
                         <x-admin.form.inputs.select label="{{__('webCaption.country.title')}}"  tooltip="{{__('webCaption.country.caption')}}" for="country_id" name="country_id"
-                                                    placeholder="{{ __('locale.country.caption') }}" customClass="country"  editSelected="{{(isset($data->country_id) && ($data->country_id != null))?$data->country_id :''; }}"  required="required" :optionData="$country" />
+                                                    placeholder="{{ __('locale.country.caption') }}" customClass="country"  editSelected="{{(isset($data->country_id) && ($data->country_id != null)) ? $data->country_id :'' }}"  required="required" :optionData="$country" />
                         @if($errors->has('country_id'))
                            <x-admin.form.form_error_messages message="{{ $errors->first('country_id') }}"  />
                         @endif
@@ -526,106 +526,5 @@
       })
    </script>
 
-
-   <script>
-
-      $(document).ready(function() {
-         var  country  = $('.country').find(":selected").val();
-         var  state  = "{{old('state_id')}}";
-         var  city  = "{{old('city_id')}}";
-
-         if(country){
-            stateList(country,state);
-         }
-
-         if(state){
-            $.ajax ({
-               type: 'POST',
-               url: "{{route('company.city-list')}}",
-               data: { id : state },
-               success : function(result) {
-                  $('#city_id').html('<option value="">Select City</option>');
-                  $.each(result.cities, function (key, value) {
-
-                     if(value.id == city){
-                        var selected_c = 'selected';
-                     }else{
-                        var selected_c = '';
-                     }
-
-                     $("#city_id").append('<option value="' + value
-                             .id + '" '+ selected_c +'>' + value.name + '</option>');
-                  });
-               }
-            });
-         }
-
-
-         $('.country').on('change', function(){
-            var selectCountry  = $(this).val();
-            stateList(selectCountry);
-         });
-
-
-         $('.state').on('change', function () {
-            var selectState  = $(this).val();
-            cityList(selectState);
-         });
-
-      });
-
-
-
-      function stateList(country , selected_state = ''){
-
-         $.ajax ({
-            type: 'POST',
-            url: "{{route('company.state-list')}}",
-            data: { id : country },
-            success : function(result) {
-
-               $('#state_id').html('<option value="">Select State</option>');
-               $.each(result.states, function (key, value) {
-
-                  if(value.id == selected_state){
-                     var selected_s = 'selected';
-                  }else{
-                     var selected_s = '';
-                  }
-                  $("#state_id").append('<option value="' + value
-                          .id + '" '+ selected_s + '>' + value.name + '</option>');
-               });
-               $('#city_id').html('<option value="">Select City</option>');
-            }
-         });
-
-      }
-
-
-      function cityList(state,selected_city =''){
-
-         $.ajax ({
-            type: 'POST',
-            url: "{{route('company.city-list')}}",
-            data: { id : state },
-            success : function(result) {
-               $('#city_id').html('<option value="">Select City</option>');
-               $.each(result.cities, function (key, value) {
-
-                  if(value.id == selected_city){
-                     var selected_c = 'selected';
-                  }else{
-                     var selected_c = '';
-                  }
-
-                  $("#city_id").append('<option value="' + value
-                          .id + '" '+ selected_c +'>' + value.name + '</option>');
-               });
-            }
-         });
-      }
-
-
-   </script>
 
 @endpush
