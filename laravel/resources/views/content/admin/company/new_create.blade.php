49b61356d5363050e4eb35c1d090bd2e171342f7
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-10 15:56:32 +0530

    Update new_create.blade.php

--- laravel/resources/views/content/admin/company/new_create.blade.php
+++ laravel/resources/views/content/admin/company/new_create.blade.php
@@ -344,26 +344,26 @@
                      </div>
                   </div>
 
-                  <div class="row">
-                     <div class="col-md-4">
-                        <div class="form-group">
-                           <x-admin.form.inputs.text id="" for="contact_1_phone" tooltip="{{__('webCaption.phone.caption')}}" label="{{__('webCaption.phone.title')}}" maxlength="20" class="form-control" name="contact_phone[]"  placeholder="{{__('webCaption.phone.title')}}" value=""  required="" />
-                           @if($errors->has('contact_phone.0'))
-                              <x-admin.form.form_error_messages message="{{ $errors->first('contact_phone.0') }}"  />
-                           @endif
-                        </div>
-                     </div>
-                     <div class="col-md-4">
-                        <x-admin.form.label for="" tooltip="{{__('webCaption.contact_option.caption')}}" value="{{__('webCaption.contact_option.title')}}" class="" />
-                        <div class="form-group">
-                           <x-admin.form.inputs.checkbox id="" for="contact_1_option_viber" tooltip="{{__('webCaption.viber.caption')}}" label="{{__('webCaption.viber.title')}}"  class="form-control" name="contact_viber[0]"   value="1" checked="{{ ( old('contact_viber.0')  == 1 )? 'checked' :'' }}" /> &ensp;
+{{--                  <div class="row">--}}
+{{--                     <div class="col-md-4">--}}
+{{--                        <div class="form-group">--}}
+{{--                           <x-admin.form.inputs.text id="" for="contact_1_phone" tooltip="{{__('webCaption.phone.caption')}}" label="{{__('webCaption.phone.title')}}" maxlength="20" class="form-control" name="contact_phone[]"  placeholder="{{__('webCaption.phone.title')}}" value=""  required="" />--}}
+{{--                           @if($errors->has('contact_phone.0'))--}}
+{{--                              <x-admin.form.form_error_messages message="{{ $errors->first('contact_phone.0') }}"  />--}}
+{{--                           @endif--}}
+{{--                        </div>--}}
+{{--                     </div>--}}
+{{--                     <div class="col-md-4">--}}
+{{--                        <x-admin.form.label for="" tooltip="{{__('webCaption.contact_option.caption')}}" value="{{__('webCaption.contact_option.title')}}" class="" />--}}
+{{--                        <div class="form-group">--}}
+{{--                           <x-admin.form.inputs.checkbox id="" for="contact_1_option_viber" tooltip="{{__('webCaption.viber.caption')}}" label="{{__('webCaption.viber.title')}}"  class="form-control" name="contact_viber[0]"   value="1" checked="{{ ( old('contact_viber.0')  == 1 )? 'checked' :'' }}" /> &ensp;--}}
 
-                           <x-admin.form.inputs.checkbox id="" for="contact_1_option_line" tooltip="{{__('webCaption.line.caption')}}" label="{{__('webCaption.line.title')}}" class="form-control" name="contact_line[0]"   value="1"  checked="{{ ( old('contact_line.0')  == 1 )? 'checked' :'' }}" />&ensp;
+{{--                           <x-admin.form.inputs.checkbox id="" for="contact_1_option_line" tooltip="{{__('webCaption.line.caption')}}" label="{{__('webCaption.line.title')}}" class="form-control" name="contact_line[0]"   value="1"  checked="{{ ( old('contact_line.0')  == 1 )? 'checked' :'' }}" />&ensp;--}}
 
-                           <x-admin.form.inputs.checkbox id="" for="contact_1_option_whatsapp" tooltip="{{__('webCaption.whatsapp.caption')}}"  label="{{__('webCaption.whatsapp.title')}}"  class="form-control" name="contact_whatsapp[0]"   value="1"  checked="{{ ( old('contact_whatsapp.0')  == 1 )? 'checked' :'' }}" />&ensp;
-                        </div>
-                     </div>
-                  </div>
+{{--                           <x-admin.form.inputs.checkbox id="" for="contact_1_option_whatsapp" tooltip="{{__('webCaption.whatsapp.caption')}}"  label="{{__('webCaption.whatsapp.title')}}"  class="form-control" name="contact_whatsapp[0]"   value="1"  checked="{{ ( old('contact_whatsapp.0')  == 1 )? 'checked' :'' }}" />&ensp;--}}
+{{--                        </div>--}}
+{{--                     </div>--}}
+{{--                  </div>--}}
 
                </div>
                <hr class="m-0 p-0">
