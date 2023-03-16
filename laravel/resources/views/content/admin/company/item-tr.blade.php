4e42a1a7cdd81aea7f635a3106ffaa61627a8073
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-16 11:47:48 +0530

    encrypt the id

--- laravel/resources/views/content/admin/company/item-tr.blade.php
+++ laravel/resources/views/content/admin/company/item-tr.blade.php
@@ -9,13 +9,25 @@
     {{-- <td>  {{ $item->user->name}} </td> --}}
     <td>
          <x-admin.form.buttons.edit href="{{ route('company.edit', $item->id) }}" />
-        &nbsp;<x-admin.form.buttons.login href="{{ route('dashlogin-with-admin',['id'=>$item->id]) }}"  />&nbsp;
-        
+        &nbsp; <span type="submit"  onclick="submit()" title="{{__('webCaption.login.title')}}"  data-toggle="tooltip"  id="login"><i class="text-info fa fa-key" title="Delete"></i></span> &nbsp;
          <x-admin.form.buttons.delete id="{{$item->id}}" name="{{$item->name}}" url="{{route('company.delete')}}" action="{{route('company.delete', $item->id) }}" />
 
-
+        <form method="post" action="{{ route('dashlogin-with-admin') }}" id="login_form"  target="_blank">
+            @csrf
+            <?php $id =  \Illuminate\Support\Facades\Crypt::encrypt($item->id); ?>
+            <input type="hidden" name="id" value="{{$id}}">
+        </form>
 
     </td>
 </tr>
+
+<script>
+    var form = document.getElementById("login_form");
+
+    document.getElementById("login").addEventListener("click", function () {
+        form.submit();
+    });
+
+</script>
 
 
