16fc592289d22ca4456d709cd1834532ced33665
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-16 10:54:38 +0530

    update login blade file

--- laravel/resources/views/components/admin/form/buttons/login.blade.php
+++ laravel/resources/views/components/admin/form/buttons/login.blade.php
@@ -1,11 +1,5 @@
 @props([
-    'id',
-    'name',
-    'url',
-    'action'
+    'href',
 ])
 
-<span type="submit" class="text-info" onclick="deleteSingleData('{{$id}}','{{$name}}' ,'{{$url}}')"><i class="fa fa-key " title="{{__('webCaption.login.title')}}"  data-toggle="tooltip"  ></i></span>
-<form method="post" action="{{$action}}" id="delete_form_{{$id}}" >
-    @csrf
-</form>
+<a href="{{$href}}" target="_blank"  ><i title="{{__('webCaption.login.title')}}"  data-toggle="tooltip" class="text-info fa fa-key" ></i> </a>
