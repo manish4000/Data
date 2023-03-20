f6e9683f17550bdc3516ff8736307054de05ac5d
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-20 18:25:19 +0530

    add email validation code

--- laravel/resources/views/components/admin/form/inputs/email.blade.php
+++ laravel/resources/views/components/admin/form/inputs/email.blade.php
@@ -16,8 +16,12 @@
 $charLength =  (isset($value))? strlen($value) : 0;
 @endphp
 
-@if (isset($label) && isset($for)) <label  @if (isset($tooltip)) title="{{$tooltip}}"  @endif   data-toggle="tooltip" for='{{ $for }}'>{{ $label }}  @if(isset($required) && !empty($required)) <span class="text-danger" style="font-size: 18px"> * </span>  @endif </label> @endif
-<input
+@if (isset($label) && isset($for)) <label  @if (isset($tooltip)) title="{{$tooltip}}"  @endif   data-toggle="tooltip" for='{{ $for }}'>{{ $label }}  @if(isset($required) && !empty($required)) <span class="text-danger" style="font-size: 14px;font-weight: bolder"> * </span>  @endif </label> @endif
+
+
+<div class="input-group">
+
+    <input
     type="email" 
     @if (isset($for)) id="{{$for}}" onkeyup="checkTextLimit('{{$for}}');"  @endif
 
@@ -30,11 +34,36 @@ $charLength =  (isset($value))? strlen($value) : 0;
     @if(isset($placeholder )) placeholder='{{ $placeholder }}'  @endif    
     @if(isset($required)) {{ $required }}  @endif    
     @if(isset($attributes)) {{$attributes}}  @endif            
-    @if(isset($value))   value="{{$value}}"  @endif   >
+    @if(isset($value))   value="{{$value}}"  @endif oninput="checker()"   >
+
+    <div class="input-group-append">
+      <span class="input-group-text" id="icon"></span>
+    </div>
+</div>
+
+
+
+
+{{-- <input
+    type="email" 
+    @if (isset($for)) id="{{$for}}" onkeyup="checkTextLimit('{{$for}}');"  @endif
+
+    @if (isset($maxlength))  maxlength="{{$maxlength}}" @endif 
+    
+    @if(isset($name)) name='{{ $name }}' @endif 
+        
+    @if(isset($class)) class="abc {{$class}} "  @endif    
+
+    @if(isset($placeholder )) placeholder='{{ $placeholder }}'  @endif    
+    @if(isset($required)) {{ $required }}  @endif    
+    @if(isset($attributes)) {{$attributes}}  @endif            
+    @if(isset($value))   value="{{$value}}"  @endif oninput="checker()"   > --}}
+
+    {{-- <p id="error-msg">Please Enter A Valid Email Id</p> --}}
 
 @if (isset($maxlength) && isset($for) )(<span class="text-right" id="count_{{$for}}">{{$charLength}}</span>/{{$maxlength}}) @endif
 
-@section('inner-script')
+@push('script')
 <script>
     function checkTextLimit(id){
         let totalCount = $('#'+id).val();
@@ -44,4 +73,34 @@ $charLength =  (isset($value))? strlen($value) : 0;
     }  
 </script>
 
-@endsection
+<script type="text/javascript">
+
+    let emailId = document.getElementById("{{$for}}");
+    let errorMsg = document.getElementById("error-msg");
+    let icon = document.getElementById("icon");
+    let mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+    
+    function checker(){
+        icon.style.display="inline-block";
+        if(emailId.value.match(mailRegex)){
+            icon.innerHTML = '<i class="fas fa-check-circle"></i>';
+            icon.style.color = '#2ecc71';
+            errorMsg.style.display = 'none';
+            emailId.style.border = '2px solid #2ecc71';
+        }
+        else if(emailId.value == ""){
+            icon.style.display = 'none';
+            errorMsg.style.display = 'none';
+            emailId.style.border = '2px solid #d1d3d4';
+        }
+        else{
+            icon.innerHTML = '<i class="fas fa-exclamation-circle"></i>';
+            icon.style.color = '#ff2851';
+            errorMsg.style.display = 'block';
+            emailId.style.border = '2px solid #ff2851';
+        }
+    
+    }
+    </script>
+
+@endpush
