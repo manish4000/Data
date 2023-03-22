04d29ee14728c0061f0b6d28b738520f751f64d4
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-22 15:13:42 +0530

    add last auth code field in users

--- laravel/database/migrations/2023_03_20_084959_add_google2fa_secret_field_in_users.php
+++ laravel/database/migrations/2023_03_20_084959_add_google2fa_secret_field_in_users.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->text('google2fa_secret')->nullable();
+            $table->text('last_auth_code')->nullable();
         });
     }
 
