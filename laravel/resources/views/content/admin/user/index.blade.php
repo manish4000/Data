ccfa5cfc7b1f070fd1f2105e503059a184e877a9
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-22 14:59:36 +0530

    add 2fa model on time of create and update

--- laravel/resources/views/content/admin/user/index.blade.php
+++ laravel/resources/views/content/admin/user/index.blade.php
@@ -49,6 +49,8 @@
 										</th>
 										<th scope="col" data-toggle="tooltip" title="{{__('webCaption.actions.caption')}}"  > {{__('webCaption.actions.title')}}
 										</th>
+										<th scope="col" data-toggle="tooltip" title="{{__('webCaption.actions.caption')}}"  > OR SCAN
+										</th>
 									</tr>
 								</thead>
 								<tbody>
@@ -75,6 +77,15 @@
 													<x-admin.form.buttons.delete id="{{$user->id}}" name="{{$user->name}}" url="{{route('users.delete')}}" action="{{route('users.delete', $user->id) }}" />   
 												@endcan
 											</td>
+											<td>
+												@if(isset($user->google2fa_secret))
+													<a  data-toggle="modal" data-id="{{$user->id}}"  class="2faModelUpdate"> <i class="fa fa-refresh fa-2x" aria-hidden="true"></i> &nbsp; <i class="fa fa-qrcode text-dark fa-2x"></i>
+													</a>
+												@else
+													<a  data-toggle="modal" data-id="{{$user->id}}"  class="2faModelCreate"> <i class="fa fa-plus fa-2x" aria-hidden="true"></i> &nbsp; <i class="fa fa-qrcode text-dark fa-2x"></i>
+													</a>
+												@endif
+											</td>
 										</tr>
 									@endforeach
 								</tbody>
@@ -94,6 +105,239 @@
 		</div>
 	</div>
 </div>
+
+{{-- model popup code  --}}
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+	  <div class="modal-content">
+		<div class="modal-header">
+		  <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+		  <button type="button" class="close" onclick="resetForm()" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		  </button>
+		</div>
+		<div class="modal-body">
+			<div class="row">
+				<div class="col-md-12 mt-4">
+					<div class="card card-default">
+						<h4 class="card-heading text-center mt-4">Set up Google Authenticator</h4>
+		
+						<div class="card-body" style="text-align: center;">
+							<p>Set up your two factor authentication by scanning the barcode below. Alternatively, you can use the code 
+						 	 <span id="2fa_secret"><strong></strong> </span> </p>
+							<div id="qr-image">
+							   
+							</div>
+							<p>You must set up your Google Authenticator app before continuing. You will be unable to login otherwise</p>
+
+							@if($errors->any())
+								<div class="col-md-12">
+									<div class="alert alert-danger">
+									<strong>{{$errors->first()}}</strong>
+									</div>
+								</div>
+						    @endif
+
+							<form class="" id="verify-form" method="POST" action="{{ route('users.verify-2fa') }}">
+								{{ csrf_field() }}
+
+								<div class="form-group">
+									
+									<label for="one_time_password" class=" control-label">One Time Password</label>
+
+									<input id="one_time_password" type="number" class="form-control" name="one_time_password" required autofocus>
+									<input value="" id="id" type="hidden" class="form-control" name="id" value="" required >
+
+									<p>Please enter the  <strong>OTP</strong> generated on your Authenticator App. <br> Ensure you submit the current one because it refreshes every 30 seconds.</p>
+								</div>
+	
+								<div class="form-group">
+									
+									<button type="submit" class="btn btn-primary">
+										Verify 
+									</button>
+									
+								</div>
+							</form>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal-footer">
+		  <button type="button" class="btn btn-secondary" onclick="resetForm()"  data-dismiss="modal">Close</button>
+		</div>
+	  </div>
+	</div>
+  </div>
+
+{{--  --}}
+{{-- model popup code  --}}
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+	  <div class="modal-content">
+		<div class="modal-header">
+		  <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+		  <button type="button" class="close" onclick="resetForm()" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		  </button>
+		</div>
+		<div class="modal-body">
+			<div class="row">
+				<div class="col-md-12 mt-4">
+					<div class="card card-default">
+						<h4 class="card-heading text-center mt-4">Set up Google Authenticator</h4>
+		
+						<div class="card-body" style="text-align: center;">
+							<div id="qr-image">
+								<img src="{{asset('assets/images/qr-code.jpg')}}" alt="">
+							</div>
+							@if($errors->any())
+								<div class="col-md-12">
+									<div class="alert alert-danger">
+									<strong>{{$errors->first()}}</strong>
+									</div>
+								</div>
+						    @endif
+
+							<form class="" id="update-verify-form" method="POST" action="{{ route('users.verify-2fa') }}">
+								{{ csrf_field() }}
+
+								<div class="form-group">
+									
+									<label for="one_time_password" class=" control-label">One Time Password</label>
+
+									<input id="one_time_password" type="number" class="form-control" name="one_time_password" required autofocus>
+									<input value="" id="update-id" type="hidden" class="form-control" name="id" value="" required >
+
+									<p>Please enter the  <strong>OTP</strong> From last scanned device to update.<br> Ensure you submit the current one because it refreshes every 30 seconds.</p>
+								</div>
+	
+								<div class="form-group">
+									
+									<button type="submit" class="btn btn-primary">
+										Verify 
+									</button>
+									
+								</div>
+							</form>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal-footer">
+		  <button type="button" class="btn btn-secondary" onclick="resetForm()"  data-dismiss="modal">Close</button>
+		</div>
+	  </div>
+	</div>
+  </div>
+
+{{--  --}}
 @include('components.admin.alerts.delete-alert-box')
 @include('components.admin.filter.order-by')
-@stop
+
+
+@push('script')
+
+<script>
+	$(".2faModelCreate").click(function(){
+		let id = $(this).attr("data-id");
+		generate(id);
+	});
+
+
+	function generate(id){
+
+		$.ajax({
+	        	url: "{{ route('users.2fa') }}",
+	        	data: {'id':id },
+	        	type: "post",
+	        	success: function(data) {
+					$('#qr-image').html(data.qr_image);
+					$('#2fa_secret').html("<strong>"+data.google2fa_secret + "</strong>");
+					$('#id').val(id);
+					$('#exampleModalCenter').modal('show');
+	        	},
+	        	error: function() {}
+	        });
+	}
+
+
+	$(".2faModelUpdate").click(function(){
+		
+		let id = $(this).attr("data-id");
+		$('#update-id').val(id);
+		$('#exampleModal').modal('show');
+
+	});
+
+    function resetForm(){
+		$('#verify-form')[0].reset();
+	}
+
+	$('#update-verify-form').on('submit' ,function(e){
+		event.preventDefault();
+
+         let usrid =	$('#update-verify-form #update-id').val();
+
+
+			$.ajax({
+			type: 'POST',
+			url: "{{ route('users.update-verify-2fa') }}",
+			data: $('#update-verify-form').serialize(),
+			success: function (data) {
+
+				if(data.result.status == true){
+					$('#exampleModal').modal('hide');
+					generate(usrid);
+				}else{
+
+				
+				}
+
+			}
+		});
+	
+	});
+
+
+
+	$('#verify-form').on('submit' ,function(e){
+		event.preventDefault();
+
+			$.ajax({
+			type: 'POST',
+			url: "{{ route('users.verify-2fa') }}",
+			data: $('#verify-form').serialize(),
+			success: function (data) {
+
+				if(data.result.status == true){
+					$('#exampleModalCenter').modal('hide');
+					successToast(data.result.message);
+				}else{
+
+					errorToast(data.result.message);
+				}
+
+			}
+		});
+	
+	});
+
+</script>
+
+@endpush
+
+
+@endsection
