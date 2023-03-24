50ac143aa42fa4b31cf1374bf43312a0bd76921f
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-24 11:34:44 +0530

    add condation in custom blade file

--- laravel/resources/views/components/dash/form/buttons/custom.blade.php
+++ laravel/resources/views/components/dash/form/buttons/custom.blade.php
@@ -4,4 +4,9 @@
     'color'
 ])
 
+@php
+$color = (isset($color)) ? $color : '';
+$value = (isset($value)) ? $value : '';
+$iconClass = (isset($iconClass)) ? $iconClass : '';
+@endphp
 <button  type="submit" class="btn {{$color}} mr-1"> <i class="{{$iconClass}}"></i> {{$value}}  </button>
