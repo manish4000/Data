2670e51681b67f9444857f2678651088cab8fdb8
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-24 12:28:38 +0530

    Update new_create.blade.php

--- laravel/resources/views/content/admin/company/new_create.blade.php
+++ laravel/resources/views/content/admin/company/new_create.blade.php
@@ -298,7 +298,7 @@
 
             </div>
          </div>
-         {{-- <div class="card">
+         <div class="card">
             <div class="card-header">
                <h4 class="card-title">
                   <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info font-medium-3 mr-1"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
@@ -382,7 +382,7 @@
             </div>
 
 
-         </div> --}}
+         </div>
 
 
          {{-- <div class="card">
