b1cfff56420b8f1ba24150bbcbac5db4cb0e2c4e
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-24 11:40:24 +0530

    update the layout of verify form

--- laravel/resources/views/google2fa/index.blade.php
+++ laravel/resources/views/google2fa/index.blade.php
@@ -1,45 +1,39 @@
+
 @extends('layouts/fullLayoutMaster')
+@section('title', 'Login Page')
+@section('page-style')
+{{-- Page Css files --}}
+<link rel="stylesheet" href="{{ asset(mix('css/base/pages/page-auth.css')) }}">
+@endsection
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center align-items-center " style="height: 70vh;S">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading font-weight-bold">Register</div>
-                    <hr>
-                    @if($errors->any())
-                        <div class="col-md-12">
-                            <div class="alert alert-danger">
-                            <strong>{{$errors->first()}}</strong>
-                            </div>
-                        </div>
-                    @endif
+<div class="auth-wrapper auth-v1 px-2">
+  <div class="auth-inner py-2">
+    <!-- Login v1 -->
+    <div class="card mb-0">
+      <div class="card-body">
+        <a href="javascript:void(0);" class="brand-logo ">
+          <img src="{{asset('assets/images/logo/logo.png')}}" height="50" alt="GABS Logo"> 
+        </a> 
+        @if ($errors->any())
+          <x-admin.form.form_error_messages message="{{$errors->first()}}" />
+        @endif
+        <form class="auth-login-form" method="POST" action="{{ route('2fa') }}">
+          {{ csrf_field() }}
+            <div class="form-group">
+                <p>Please enter the  <strong>OTP</strong> generated on your Authenticator App.</p>
+                <label for="one_time_password" class="col-md-12 control-label">One Time Password</label>
 
-                    <div class="panel-body">
-                        <form class="form-horizontal" method="POST" action="{{ route('2fa') }}">
-                            {{ csrf_field() }}
-
-                            <div class="form-group">
-                                <p>Please enter the  <strong>OTP</strong> generated on your Authenticator App. <br> Ensure you submit the current one because it refreshes every 30 seconds.</p>
-                                <label for="one_time_password" class="col-md-4 control-label">One Time Password</label>
-
-
-                                <div class="col-md-6">
-                                    <input id="one_time_password" type="number" class="form-control" name="one_time_password" required autofocus>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4 mt-3">
-                                    <button type="submit" class="btn btn-primary">
-                                        Login
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                <div class="col-md-12">
+                    <input id="one_time_password" type="number" class="form-control" name="one_time_password" required autofocus>
                 </div>
             </div>
-        </div>
+          <button type="submit" class="btn btn-primary btn-block my-3" tabindex="4">Verify</button>
+        </form>
+
+      </div>
     </div>
+    <!-- /Login v1 -->
+  </div>
+</div>
 @endsection
