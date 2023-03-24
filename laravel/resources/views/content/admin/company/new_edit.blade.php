d5c37ff5041b59547ee76afd63930e7ea36e901e
manisheximgroup <manisheximgroup1@gmail.com>
2023-03-24 12:36:09 +0530

    Update new_edit.blade.php

--- laravel/resources/views/content/admin/company/new_edit.blade.php
+++ laravel/resources/views/content/admin/company/new_edit.blade.php
@@ -380,7 +380,7 @@
     </div> --}}
 
 
-    {{-- <div class="card">
+    <div class="card">
         <div class="card-header">
             <h4 class="card-title">
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe font-medium-3 mr-1"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>
@@ -437,7 +437,7 @@
  
 
       </div>
-    </div> --}}
+    </div>
 
     <div class="form-group">
 
