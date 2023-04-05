197aa0a1a1b08ba0c5b365d86216e7f20dbd6649
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-05 15:10:35 +0530

    add id and class

--- laravel/resources/views/components/dash/form/buttons/update.blade.php
+++ laravel/resources/views/components/dash/form/buttons/update.blade.php
@@ -1,1 +1,10 @@
-<button  type="submit" class="btn btn-success mr-1"> <i class="fa fa-pencil"></i> {{__('webCaption.update.title')}}  </button>
+@props([
+    'id',
+    'customClass',
+])
+
+@php
+$customClass = (isset($customClass)) ? $customClass :'';
+@endphp
+
+<button  @if(isset($id))  id="{{$id}}" @endif  type="submit" class="btn btn-success {{$customClass}} mr-1"> <i class="fa fa-pencil"></i> {{__('webCaption.update.title')}}  </button>
