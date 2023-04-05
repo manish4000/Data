94a71843d177e46b7b13ca2441b15b3974710473
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-05 15:09:04 +0530

    add model to save otp details

---
+++ laravel/app/Models/Dash/ComapnyBankDetailsOtpVerify.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Dash;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ComapnyBankDetailsOtpVerify extends Model
+{
+    use HasFactory;
+    protected $table = 'company_bank_details_otp_verify';
+}
