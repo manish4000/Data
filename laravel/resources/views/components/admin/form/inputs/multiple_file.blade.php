0fe4b62494182ff649ee9852900f630d7ee8a1e0
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-06 15:24:49 +0530

    remove value

--- laravel/resources/views/components/admin/form/inputs/multiple_file.blade.php
+++ laravel/resources/views/components/admin/form/inputs/multiple_file.blade.php
@@ -8,5 +8,5 @@
 
 <div class="form-group">
     <label for='{{ $for }}'>{{ $lable }} </label>
-    <input id='{{ $for }}' name='{{ $name }}' type="text" class='form-control' placeholder='{{ $placeholder }}' {{$attributes}} value='{{ old($name, $value) }}' >
+    <input id='{{ $for }}' name='{{ $name }}' type="file" class='form-control' placeholder='{{ $placeholder }}' {{$attributes}} value='{{ old($name) }}' multiple>
 </div>
