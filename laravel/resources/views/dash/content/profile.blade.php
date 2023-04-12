01b1e44c840071bfbf97244af926bdc5fb4a0268
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-12 18:10:58 +0530

    add blank page for createing company profile

---
+++ laravel/resources/views/dash/content/profile.blade.php
@@ -0,0 +1,12 @@
+@extends('dash/layouts/LayoutMaster')
+
+@section('title', __('webCaption.edit_profile.title') )
+
+@section('content')
+<div>
+        <div class="text-center">
+            <h2>Start creating Company profile form from Here  😐 </h2>
+        </div>
+</div>
+@endsection
+
