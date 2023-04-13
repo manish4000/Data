cf0f4fd438f7d376ec9dcc1ddae38638193c7d38
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-13 14:59:22 +0530

    update migration table name

--- laravel/database/migrations/dash/2023_04_13_092232_add_deleted_at_in_company_plan_permissions.php
+++ laravel/database/migrations/dash/2023_04_13_092232_add_deleted_at_in_company_plan_permissions.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('company_company_plan_permissions', function (Blueprint $table) {
+        Schema::table('company_plan_permissions', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('company_company_plan_permissions', function (Blueprint $table) {
+        Schema::table('company_plan_permissions', function (Blueprint $table) {
             //
         });
     }
