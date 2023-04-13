5d77583fec79072e20358dc64c9dccb6a4111f3f
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-13 15:41:57 +0530

    update

--- laravel/resources/views/dash/content/company/profile.blade.php
+++ laravel/resources/views/dash/content/company/profile.blade.php
@@ -122,7 +122,7 @@
             </div>
          </div>
       </div>
-      {{-- <div class="card card-primary">
+      <div class="card card-primary">
          <div class="card-header">
             <h4 class="card-title">
                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map font-medium-3 mr-1">
@@ -288,7 +288,7 @@
             </div>
          </div>
       </div>
-      <div class="card card-primary">
+      {{-- <div class="card card-primary">
          <div class="card-header">
             <h4 class="card-title">
                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers font-medium-3 mr-1">
