0137f52d05b333a4711028d0b1c85e227c0e9bd6
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-15 16:31:55 +0530

    group the route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -41,11 +41,9 @@ Auth::routes();
 Route::get('google-v3-recaptcha', [GoogleV3CaptchaController::class, 'index']);
 Route::post('validate-g-recaptcha', [GoogleV3CaptchaController::class, 'validateGCaptch']);
 
-
     Route::post('/2fa', function () {
         return redirect()->route('dashboard');
     })->name('2fa');
-
 
 Route::get('/complete-registration', [RegisterController::class, 'completeRegistration'])->name('complete.registration');
 
@@ -69,22 +67,24 @@ Route::group([ 'middleware' => ['auth'], 'prefix' => 'admin' ], function() {
 
     Route::get('data-migrate','Admin\DataImportController@importData');
 
-    Route::post('users/login-from-admin','Admin\UserController@loginFromAdmin')->name('users.login-form-admin');
-    Route::post('users/delete','Admin\UserController@destroy')->name('users.delete');
-    Route::post('users/delete-multiple','Admin\UserController@deleteMultiple')->name('users.delete-multiple');
-   
+    Route::group(['as' => 'users.','prefix' => 'users'] ,function(){
+
+        Route::post('login-from-admin','Admin\UserController@loginFromAdmin')->name('login-form-admin');
+        Route::post('delete','Admin\UserController@destroy')->name('delete');
+        Route::post('delete-multiple','Admin\UserController@deleteMultiple')->name('delete-multiple');
+        Route::get('edit/{id}', 'Admin\UserController@edit')->name('edit');
+        Route::get('permission/{id}', 'Admin\UserController@showPermission')->name('permission');
+        Route::post('permission/update', 'Admin\UserController@updatePermission')->name('update-permission');
+        Route::get('update-status/{id}','Admin\UserController@updateStatus')->name('update-status');
+        Route::post('add-two-step-verification','Admin\UserController@addTwoStapVerification')->name('2fa');
+        Route::get('delete-two-step-verification/{id}','Admin\UserController@deleteTwoStapVerification')->name('delete-2fa');
+        Route::post('update-two-step-verification','Admin\UserController@updateTwoStapVerification')->name('2fa-update');
+        Route::post('verify-two-step-verification','Admin\UserController@verifyTwoStapVerification')->name('verify-2fa');
+        Route::post('update-verify-two-step-verification','Admin\UserController@updateverifyTwoStapVerification')->name('update-verify-2fa');
+
+    });
+
     Route::resource('users', 'Admin\UserController');
-    Route::get('users/edit/{id}', 'Admin\UserController@edit')->name('users.edit');
-    Route::get('users/permission/{id}', 'Admin\UserController@showPermission')->name('users.permission');
-    Route::post('users/permission/update', 'Admin\UserController@updatePermission')->name('users.update-permission');
-    Route::get('users/update-status/{id}','Admin\UserController@updateStatus')->name('users.update-status');
-    Route::post('users/add-two-step-verification','Admin\UserController@addTwoStapVerification')->name('users.2fa');
-    Route::get('users/delete-two-step-verification/{id}','Admin\UserController@deleteTwoStapVerification')->name('users.delete-2fa');
-    Route::post('users/update-two-step-verification','Admin\UserController@updateTwoStapVerification')->name('users.2fa-update');
-    Route::post('users/verify-two-step-verification','Admin\UserController@verifyTwoStapVerification')->name('users.verify-2fa');
-    Route::post('users/update-verify-two-step-verification','Admin\UserController@updateverifyTwoStapVerification')->name('users.update-verify-2fa');
-
-
     Route::resource('roles', 'Admin\RoleController');
 
     Route::post('site-languages/delete-multiple', [SiteLanguageController::class, 'deleteMultiple'])->name('site-languages.delete-multiple');
@@ -140,7 +140,7 @@ Route::group([ 'middleware' => ['auth'], 'prefix' => 'admin' ], function() {
     //     Route::post('/check-shortname-exists', [CompanyController::class, 'checkShortNameExists'])->name('check-shortname-exists');
     // });
 
-    Route::group(['prefix' => 'company','as'=> 'company.' ,'namespace' => 'Admin'], function () {
+    Route::group(['prefix' => 'company','as'=> 'company.' ,'namespace' => 'Admin'], function () { 
 
         Route::get('/','CompanyController@index')->name('index');
         Route::get('/import-company-data','CompanyController@importDataFromJct');
@@ -158,53 +158,57 @@ Route::group([ 'middleware' => ['auth'], 'prefix' => 'admin' ], function() {
 
         Route::post('state-list','CompanyController@stateList')->name('state-list');
         Route::post('city-list','CompanyController@cityList')->name('city-list');
+ 
+        Route::group(['namespace' => 'Company'],function(){
 
-        Route::group(['prefix' =>'permission' ,'as' => 'permission.','namespace' => 'Company' ],function(){
-            Route::get('/', 'PermissionController@index')->name('index');
-            Route::get('/create', 'PermissionController@create')->name('create');
-            Route::post('/store', 'PermissionController@store')->name('store');
-            Route::get('edit/{id}','PermissionController@edit')->name('edit');
-            Route::post('/delete', 'PermissionController@destroy')->name('delete');
-            Route::post('/position','PermissionController@updateCompanyPermissionPosition')->name('update-position');   
-        });
+            Route::group(['prefix' =>'permission' ,'as' => 'permission.' ],function(){
+                Route::get('/', 'PermissionController@index')->name('index');
+                Route::get('/create', 'PermissionController@create')->name('create');
+                Route::post('/store', 'PermissionController@store')->name('store');
+                Route::get('edit/{id}','PermissionController@edit')->name('edit');
+                Route::post('/delete', 'PermissionController@destroy')->name('delete');
+                Route::post('/position','PermissionController@updateCompanyPermissionPosition')->name('update-position');   
+            });
 
-        Route::group(['prefix' =>'plans' ,'as' => 'plans.','namespace' => 'Company' ],function(){
-            Route::get('/', 'PlanController@index')->name('index');
-            Route::get('/create', 'PlanController@create')->name('create');
-            Route::post('/store', 'PlanController@store')->name('store');
-            Route::get('edit/{id}','PlanController@edit')->name('edit');
-            Route::post('/delete', 'PlanController@destroy')->name('delete');   
-            Route::post('/delete-multiple','PlanController@deleteMultiple')->name('delete-multiple');
-        });
-        Route::group(['prefix' =>'plan-permission' ,'as' => 'plansPermission.','namespace' => 'Company' ],function(){
-            Route::get('/', 'PlanPermissionController@index')->name('index');
-            Route::post('/store', 'PlanPermissionController@store')->name('store');
-        });
+            Route::group(['prefix' =>'plans' ,'as' => 'plans.' ],function(){
+                Route::get('/', 'PlanController@index')->name('index');
+                Route::get('/create', 'PlanController@create')->name('create');
+                Route::post('/store', 'PlanController@store')->name('store');
+                Route::get('edit/{id}','PlanController@edit')->name('edit');
+                Route::post('/delete', 'PlanController@destroy')->name('delete');   
+                Route::post('/delete-multiple','PlanController@deleteMultiple')->name('delete-multiple');
+            });
 
-        Route::group(['prefix' =>'module' ,'as' => 'module.','namespace' => 'Company' ],function(){
-            Route::get('/', 'ModuleController@index')->name('index');
-            Route::get('/create', 'ModuleController@create')->name('create');
-            Route::post('/store', 'ModuleController@store')->name('store');
-            Route::get('edit/{id}','ModuleController@edit')->name('edit');
-            Route::post('/delete', 'ModuleController@destroy')->name('delete');   
-            Route::post('/delete-multiple','ModuleController@deleteMultiple')->name('delete-multiple');
-        });
-
-        Route::group(['prefix' =>'menu-groups' ,'as' => 'menu-groups.','namespace' => 'Company' ],function(){
-            Route::get('/', 'MenuGroupController@index')->name('index');
-            Route::get('/create', 'MenuGroupController@create')->name('create');
-            Route::post('/store', 'MenuGroupController@store')->name('store');
-            Route::get('edit/{id}','MenuGroupController@edit')->name('edit');
-            Route::post('/delete', 'MenuGroupController@destroy')->name('delete');   
-            Route::get('{id}/menus', 'MenuGroupController@menus')->name('menus');
-            Route::post('add-menu', 'MenuGroupController@addMenu')->name('add-menu');
-            Route::get('menu/{id}/edit', 'MenuGroupController@editMenu')->name('menu.edit');
-            Route::post('menu/{id}/update', 'MenuGroupController@updateMenu')->name('menu.update');
-            Route::post('delete-menu', 'MenuGroupController@destroyMenu')->name('menu.delete');
-            Route::post('/delete-multiple','MenuGroupController@deleteMultiple')->name('delete-multiple'); 
-            Route::post('update-menu-position','MenuGroupController@updateMenuPosition')->name('update-menu-position');
-
-        });
+            Route::group(['prefix' =>'plan-permission' ,'as' => 'plansPermission.' ],function(){
+                Route::get('/', 'PlanPermissionController@index')->name('index');
+                Route::post('/store', 'PlanPermissionController@store')->name('store');
+            });
+            
+            Route::group(['prefix' =>'module' ,'as' => 'module.'],function(){
+                Route::get('/', 'ModuleController@index')->name('index');
+                Route::get('/create', 'ModuleController@create')->name('create');
+                Route::post('/store', 'ModuleController@store')->name('store');
+                Route::get('edit/{id}','ModuleController@edit')->name('edit');
+                Route::post('/delete', 'ModuleController@destroy')->name('delete');   
+                Route::post('/delete-multiple','ModuleController@deleteMultiple')->name('delete-multiple');
+            });
+            
+            Route::group(['prefix' =>'menu-groups' ,'as' => 'menu-groups.'],function(){
+                Route::get('/', 'MenuGroupController@index')->name('index');
+                Route::get('/create', 'MenuGroupController@create')->name('create');
+                Route::post('/store', 'MenuGroupController@store')->name('store');
+                Route::get('edit/{id}','MenuGroupController@edit')->name('edit');
+                Route::post('/delete', 'MenuGroupController@destroy')->name('delete');   
+                Route::get('{id}/menus', 'MenuGroupController@menus')->name('menus');
+                Route::post('add-menu', 'MenuGroupController@addMenu')->name('add-menu');
+                Route::get('menu/{id}/edit', 'MenuGroupController@editMenu')->name('menu.edit');
+                Route::post('menu/{id}/update', 'MenuGroupController@updateMenu')->name('menu.update');
+                Route::post('delete-menu', 'MenuGroupController@destroyMenu')->name('menu.delete');
+                Route::post('/delete-multiple','MenuGroupController@deleteMultiple')->name('delete-multiple'); 
+                Route::post('update-menu-position','MenuGroupController@updateMenuPosition')->name('update-menu-position');
+    
+            });
+        }); 
 
     });
 
@@ -248,43 +252,35 @@ Route::group([ 'middleware' => ['auth'], 'prefix' => 'admin' ], function() {
 
 
     /* Route Masters */
-    Route::group(['prefix' => 'masters'], function () {
+    Route::group(['prefix' => 'masters' ,'as' => 'masters.'], function () {
 
-        // Route::get('/vehicle/{model}', [VechileMasterController::class, 'index'])->name('masters-vehicle'); 
-        // Route::post('/vehicle/{model}/store', [VechileMasterController::class, 'store'])->name('masters-vehicle-store');
-        // Route::post('/vehicle/{model}/update-status', [VechileMasterController::class, 'updateStatus'])->name('masters-vehicle-update-status');      
-        // Route::post('/vehicle/{model}/delete/{id}', [VechileMasterController::class, 'destroy'])->name('masters-vehicle-delete');
-        // Route::get('/vehicle/{model}/edit/{id}', [VechileMasterController::class, 'edit'])->name('masters-vehicle-edit');
-        // Route::post('/vehicle/{model}/getChildList', [VechileMasterController::class,'getChildList']);     
-
-
-
-        Route::group(['prefix' => 'vehicle'],function(){
+        Route::group(['prefix' => 'vehicle' ,'as' => 'vehicle.'],function(){
             //routes of type module 
-            Route::group(['prefix' => 'type'],function(){
-                Route::get('/', [TypeController::class, 'index'])->name('masters-vehicle-type'); 
-                Route::post('/store', [TypeController::class, 'store'])->name('masters-vehicle-type-store');
-                Route::post('/update-status', [TypeController::class, 'updateStatus'])->name('masters-vehicle-type-update-status');      
-                Route::post('/delete', [TypeController::class, 'destroy'])->name('masters-vehicle-type-delete');
-                Route::get('/edit/{id}', [TypeController::class, 'edit'])->name('masters-vehicle-type-edit');
-                Route::get('/add', [TypeController::class, 'add'])->name('masters-vehicle-type-add');
-                Route::get('/create', [TypeController::class, 'create'])->name('masters-vehicle-type-create');
+            Route::group(['prefix' => 'type','as' => 'type.'],function(){
+
+                Route::get('/', [TypeController::class, 'index'])->name('index'); 
+                Route::post('/store', [TypeController::class, 'store'])->name('store');
+                Route::post('/update-status', [TypeController::class, 'updateStatus'])->name('update-status');      
+                Route::post('/delete', [TypeController::class, 'destroy'])->name('delete');
+                Route::get('/edit/{id}', [TypeController::class, 'edit'])->name('edit');
+                Route::get('/add', [TypeController::class, 'add'])->name('add');
+                Route::get('/create', [TypeController::class, 'create'])->name('create');
                 Route::post('/getChildList', [TypeController::class,'getChildList']);
-                Route::post('/delete-multiple', [TypeController::class,'deleteMultiple'])->name('masters-vehicle-type-delete-multiple');
+                Route::post('/delete-multiple', [TypeController::class,'deleteMultiple'])->name('delete-multiple');
             });
 
             //routes of make module 
 
-            Route::group(['prefix' => 'make'],function(){
-                Route::get('/', [MakeController::class, 'index'])->name('masters-vehicle-make'); 
-                Route::post('/store', [MakeController::class, 'store'])->name('masters-vehicle-make-store');
-                Route::post('/update-status', [MakeController::class, 'updateStatus'])->name('masters-vehicle-make-update-status');      
-                Route::post('/delete', [MakeController::class, 'destroy'])->name('masters-vehicle-make-delete');
-                Route::get('/edit/{id}', [MakeController::class, 'edit'])->name('masters-vehicle-make-edit');
-                Route::get('/add', [MakeController::class, 'add'])->name('masters-vehicle-make-add');
-                Route::get('/create', [MakeController::class, 'create'])->name('masters-vehicle-make-create');
+            Route::group(['prefix' => 'make' , 'as' => 'make.'],function(){
+                Route::get('/', [MakeController::class, 'index'])->name('index'); 
+                Route::post('/store', [MakeController::class, 'store'])->name('store');
+                Route::post('/update-status', [MakeController::class, 'updateStatus'])->name('update-status');      
+                Route::post('/delete', [MakeController::class, 'destroy'])->name('delete');
+                Route::get('/edit/{id}', [MakeController::class, 'edit'])->name('edit');
+                Route::get('/add', [MakeController::class, 'add'])->name('add');
+                Route::get('/create', [MakeController::class, 'create'])->name('create');
                 Route::post('/getChildList', [MakeController::class,'getChildList']);
-                Route::post('/delete-multiple', [MakeController::class,'deleteMultiple'])->name('masters-vehicle-make-delete-multiple');
+                Route::post('/delete-multiple', [MakeController::class,'deleteMultiple'])->name('delete-multiple');
             });
         });
 
@@ -292,17 +288,3 @@ Route::group([ 'middleware' => ['auth'], 'prefix' => 'admin' ], function() {
     });
 
 });
-
-
-
-
-// Set Locale
-// $webLanguages = \App\Models\SiteLanguage::get(['alias']);
-// if ($webLanguages) {
-//     foreach ( $webLanguages as $lang ) {
-//         Route::get($lang->alias, function() use ($lang){
-//             session(['locale' => $lang->alias]);
-//             return back();
-//         });
-//     }
-// }
