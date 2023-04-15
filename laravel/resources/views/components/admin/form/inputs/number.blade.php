39b2d02425b049a9bba4e6aabae5a2b8f2eba2ae
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-15 13:27:19 +0530

    add input type number file

---
+++ laravel/resources/views/components/admin/form/inputs/number.blade.php
@@ -0,0 +1,57 @@
+@props([
+    'for',
+    'label',
+    'name',
+    'placeholder',
+    'value',
+    'required',
+    'customClass',
+    'maxlength',
+    'attributes',
+    'tooltip',
+    'readonly'
+  
+])
+@php
+
+$charLength =  (isset($value))? strlen($value) : 0;
+$customClass = isset($customClass) ? $customClass : '';
+$readonly = isset($readonly) ? $readonly : '';
+@endphp
+
+@if (isset($label) && isset($for)) 
+  <label  @if (isset($tooltip)) title="{{$tooltip}}"  @endif   data-toggle="tooltip" for='{{ $for }}'>{{ $label }}  @if(isset($required) && !empty($required)) <span class="text-danger" style="font-size: 14px;font-weight: bolder"> * </span>  @endif </label>
+@endif
+
+    @if (isset($maxlength) && isset($for) )
+        <div class="character-counter-div">
+        (<span class="text-right" id="count_{{$for}}">{{$charLength}}</span>/{{$maxlength}})
+        </div>
+    @endif
+    
+ <input
+    type="number" 
+    @if (isset($for)) id="{{$for}}" onkeyup="checkTextLimit('{{$for}}');"  @endif
+
+    @if (isset($maxlength))  maxlength="{{$maxlength}}" @endif 
+    
+    @if(isset($name)) name='{{ $name }}' @endif 
+        
+     class="form-control abc {{$customClass}}"     
+
+    @if(isset($placeholder )) placeholder='{{ $placeholder }}'  @endif    
+    @if(isset($required)) {{ $required }}  @endif    
+    @if(isset($attributes)) {{$attributes}}  @endif            
+    @if(isset($value))   value="{{$value}}"  @endif {{$readonly}}  >
+
+
+
+@if(isset($name)) 
+
+<div class="m-0">
+    @if($errors->has($name))
+    <x-admin.form.form_error_messages message="{{ $errors->first($name) }}"  />
+    @endif
+</div>
+
+@endif 
