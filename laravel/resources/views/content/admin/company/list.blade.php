9987180e95aac2058e47b5a750b070f49886fe34
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-15 13:28:33 +0530

    add search of plan, business type, and status

--- laravel/resources/views/content/admin/company/list.blade.php
+++ laravel/resources/views/content/admin/company/list.blade.php
@@ -18,52 +18,48 @@
                 <div class="card-body">
                     <form method="GET" action="{{route('company.index')}}">
 
-                        <div class="d-flex justify-content-between align-items-center row pt-0 pb-2">
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <x-admin.form.inputs.text id="searchKeyword" for="{{__('webCaption.keyword.title')}}" label="{{__('webCaption.keyword.title')}}" tooltip="{{__('webCaption.keyword.caption')}}"  class="form-control" name="search[keyword]"  placeholder="{{__('webCaption.keyword.title')}}" value="{{ request()->input('search.keyword') }}"  required="" />
-
+                        <div class="row">
+                            <div class="col-md-9">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <x-admin.form.inputs.text id="searchKeyword" for="{{__('webCaption.keyword.title')}}" label="{{__('webCaption.keyword.title')}}" tooltip="{{__('webCaption.keyword.caption')}}"  class="form-control" name="search[keyword]"  placeholder="{{__('webCaption.keyword.title')}}" value="{{ request()->input('search.keyword') }}"  required="" />
+        
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <x-admin.form.inputs.select tooltip="{{__('webCaption.country.caption')}}" label="{{__('webCaption.country.title')}}" id="search[country]"  for="search[country]" name="search[country]" value="" editSelected="{{request()->input('search.country')}}" required="" :optionData="$country" />
+        
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <x-admin.form.inputs.select tooltip="{{__('webCaption.status.caption')}}" label="{{__('webCaption.status.title')}}" id="search[status]"  for="search[status]" name="search[status]" value="" editSelected="{{request()->input('search.status')}}" required="" :optionData="$status" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <x-admin.form.inputs.select tooltip="{{__('webCaption.plan.caption')}}" label="{{__('webCaption.plan.title')}}" id="search[plan]"  for="search[plan]" name="search[plan]" value="" editSelected="{{request()->input('search.plan')}}" required="" :optionData="$plans" />
+        
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <x-admin.form.inputs.select tooltip="{{__('webCaption.business_type.caption')}}" label="{{__('webCaption.business_type.title')}}" id="search[business_type]"  for="search[business_type]" name="search[business_type]" value="" editSelected="{{request()->input('search.business_type')}}" required="" :optionData="$BusinessTypes" />
+                                        </div>
+                                    </div>
+                                    
                                 </div>
                             </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-
-                                    {{-- <x-admin.form.inputs.select tooltip="{{__('webCaption.country.caption')}}" id="search[country]"  label="{{__('webCaption.country.title')}}" for="search[country]" name="company_module_id" placeholder="{{__('webCaption.country.title')}}" :optionData="$country" editSelected=""   required=""   /> --}}
-
-                                    <x-admin.form.inputs.select tooltip="{{__('webCaption.country.caption')}}" label="{{__('webCaption.country.title')}}" id="search[country]"  for="search[country]" name="search[country]" value="" editSelected="{{request()->input('search.country')}}" required="" :optionData="$country" />
-
-                                    {{-- <div class="">
-                                        <label for="keyword">Country</label>
-                                        <select class="form-control select2" name="search[country]" id="">
-                                            <option value="">select Country</option>
-                                            @foreach($country as $value)
-                                                <option value="{{$value->id}}"  {{ (request()->input('search.country') == $value->id ) ? 'selected' :'';}}> {{$value->name}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div> --}}
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    {{-- <label for="displayStatusLabel">Status</label>
-                                    <div class="">
-                                       
-                                        <select class="form-control select2" name="search[status]" id="">
-                                            <option value="">Select Status</option>
-                                            @foreach($status as $stat)
-                                                <option value="{{$stat->value}}"  {{ (request()->input('search.status') == $stat->value ) ? 'selected' :'';}}> {{$stat->name}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div> --}}
-                                    <x-admin.form.inputs.select tooltip="{{__('webCaption.status.caption')}}" label="{{__('webCaption.status.title')}}" id="search[status]"  for="search[status]" name="search[status]" value="" editSelected="{{request()->input('search.status')}}" required="" :optionData="$status" />
-                                </div>
-                            </div>
-                            <div class="col-md-3">
+                            
+                            <div class="col-md-3 mt-2">
                                 <x-admin.form.buttons.search /> 
                                 <x-admin.form.buttons.reset href="{{route('company.index')}}"/>
                             </div>
-
-
                         </div>
                     </form>
                 </div>
