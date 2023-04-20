4bc95cdced41a92d3c9bcc9189b445689e3362d7
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-20 14:21:49 +0530

    fuel module added

---
+++ laravel/database/migrations/2022_11_30_073611_create_fuel_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fuel', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique()->index();
+            $table->bigInteger('parent_id')->nullable();
+            $table->enum('display', ['Yes', 'No'])->default('Yes');
+            $table->longText('title_languages')->nullable();   
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fuel');
+    }
+}
