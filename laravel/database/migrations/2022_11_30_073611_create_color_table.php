5b91e9e82e40a6019f9f9b7f236ba92dd6cf13fa
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-20 14:20:15 +0530

    color module added

---
+++ laravel/database/migrations/2022_11_30_073611_create_color_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateColorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('color', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique()->index();
+            $table->bigInteger('parent_id')->nullable();
+            $table->enum('display', ['Yes', 'No'])->default('Yes');
+            $table->longText('title_languages')->nullable();   
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('color');
+    }
+}
