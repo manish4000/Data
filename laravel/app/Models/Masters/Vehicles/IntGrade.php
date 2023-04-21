23275d55e4200b521d487f856bc19c4484b63b7e
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-21 18:18:38 +0530

    int grade module added

---
+++ laravel/app/Models/Masters/Vehicles/IntGrade.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models\Masters\Vehicles;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\MasterDataTrait;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class IntGrade extends Model
+{
+    use HasFactory,SoftDeletes,MasterDataTrait;
+
+    protected $table = 'int_grade';
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['name','title_languages','display', 'parent_id'];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i',
+        'updated_at' => 'datetime:Y-m-d H:i',
+        'title_languages' => 'json'
+    ];
+
+    public function parent() {
+        return $this->belongsToOne(__CLASS__, 'parent_id', 'id');
+    }
+
+  //each category might have multiple children
+    public function children() {
+        return $this->hasMany(__CLASS__, 'parent_id')->orderBy('name', 'asc');
+    }
+
+    public function scopeParentOnlyFilter($query)
+    {   
+        return $query->where( 'parent_id',null)->orWhere('parent_id', 0);
+    }
+
+    public function scopeParentIdFilter($query, $parent_id)
+    {
+        return $query->where( 'parent_id', $parent_id);
+    }
+
+    public function scopeDisplayStatusFilter($query, $displayStatus)
+    {
+        return $query->where( 'display', $displayStatus);
+    }
+
+    public function scopeKeywordFilter($query, $keyword)
+    {
+        return $query->where( function($query) use ($keyword) {
+            $query->where('name', 'like', '%'.$keyword.'%')->orWhereHas( 'children', function($query) use ($keyword) {
+                    $query->where('name', 'like', '%'.$keyword.'%');
+            } );
+        });
+    }
+}
