d28b09c76b94a9f50dc5d8c606d6d53ec5cfd93e
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-24 18:14:24 +0530

    ports module add

---
+++ laravel/app/Http/Controllers/Admin/Common/PortsController.php
@@ -0,0 +1,284 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Common;
+
+use App\Http\Controllers\Controller;
+use App\Models\Ports;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\UrlGenerator;
+
+use App\Models\SiteLanguage;
+use Illuminate\Support\Facades\Auth;
+
+class PortsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    protected $baseUrl      =   '';
+    protected $url;
+    public $menuUrl ='admin/common/ports';
+
+
+    public function __construct(UrlGenerator $url) {
+        $this->url = $url;
+        $this->baseUrl =  $this->url->to('/admin/common/ports');
+    }
+
+    public function index(Request $request)
+    {    
+        if (!Auth::user()->can('common-ports')) {
+            abort(403);
+        }
+
+        $pageConfigs = [
+            'baseUrl' => $this->baseUrl, 
+            'moduleName' => __('webCaption.menu_common_ports.title'), 
+        ];
+
+        if (Auth::user()->can('common-ports-add')) {
+            $breadcrumbs[0] = [
+                'link' => $this->baseUrl.'/add',
+                'name' => __('webCaption.add.title'), 
+            ];
+        }else{
+            $breadcrumbs[0] = [];
+        }
+        
+        $data = Ports::withCount('children');
+
+        if(  $request->has('search.keyword')) {
+            $data->keywordFilter($request->input('search.keyword')); 
+        }
+        if(  $request->has('search.displayStatus') && !empty($request->input('search.displayStatus'))) {
+            $data->displayStatusFilter($request->input('search.displayStatus')); 
+        }   
+        
+       // if(  !$request->has('search.parentOnlyShowAll')) {
+            $data->parentOnlyFilter();
+        //}
+        if($request->has('order_by') &&  $request->has('order') ){
+            $data->orderBy($request->order_by, $request->order);
+        }
+
+        $data->with(['children'=>function($query) use ($request) {
+            if(  $request->has('search.keyword')) {
+                $query->keywordFilter($request->input('search.keyword')); 
+            }
+            if(  $request->has('search.displayStatus') && !empty($request->input('search.displayStatus'))) {
+                $query->displayStatusFilter($request->input('search.displayStatus')); 
+            }
+
+            if($request->has('order_by') != "children_count"){
+                
+                if($request->has('order_by') &&  $request->has('order') ){
+                    $query->orderBy($request->order_by, $request->order);
+                }
+            }
+
+        } ]); 
+
+        $perPage =  (isset($request->perPage) && !empty($request->perPage)) ? $request->perPage : 100;
+
+        $data = $data->paginate($perPage);
+
+        return view('content.admin.masters.common.ports.list', ['pageConfigs' => $pageConfigs, 'breadcrumbs' => $breadcrumbs, 'data'=>$data,'perPage' => $perPage]);
+    }
+
+
+    //send id as value because dynamic select work with  id as value  name as name  
+
+    public function add() {
+
+        if (!Auth::user()->can('common-ports-add')) {
+            abort(403);
+        }
+        $parent_data = Ports::select('id as value', 'name', 'parent_id', 'display')->orderBy('name', 'ASC')->where('parent_id', '0')->get();
+        $data = array();
+        $breadcrumbs[0] = [
+            'link' => $this->baseUrl,
+            'name' => __('webCaption.list.title')
+        ];
+        return view('content.admin.masters.common.ports.create-form',['data' => $data ,'menuUrl' =>$this->menuUrl,'breadcrumbs' =>$breadcrumbs ,'parent_data' => $parent_data  ]);
+    }
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+          
+        if($request->id){
+            if (!Auth::user()->can('common-ports-edit')) {
+                abort(403);
+            }
+            $ports_model = Ports::find($request->id);
+        }else{
+            if (!Auth::user()->can('common-ports-add')) {
+                abort(403);
+            }
+            $ports_model =   new Ports;
+        }
+
+        $validator = Validator::make($request->all(),
+          [
+            'display' => 'required',
+            'name' => 'required|unique:ports,name,'.$request->id.',id,deleted_at,NULL', 
+          ]  ,
+          [
+            'name.required' => __('webCaption.validation_required.title', ['field'=> __('webCaption.name.title')  ] ),
+            'display.required' => __('webCaption.validation_required.title', ['field'=> __('webCaption.display.title')  ] ),
+            'name.unique' => __('webCaption.validation_unique.title', ['field'=> $request->input('name')] ),
+          ]);
+    
+        if ($validator->fails()) {
+            return redirect()->back()->with('errors', $validator->errors() )->withInput();
+        }
+
+        
+
+                $ports_model->name       =   $request->name;
+                $ports_model->parent_id  =   isset($request->parent_id)? $request->parent_id : 0 ;
+                $ports_model->display    =   $request->display;
+                // $ports_model->title_languages    =   $request->title_languages;
+                
+                if($ports_model->save()){
+                    $message = (isset($request->id)) ? $request->name." ". __('webCaption.alert_updated_successfully.title') : $request->name." ".__('webCaption.alert_added_successfully.title') ;
+                    return redirect()->route('common.ports.index')->with('success_message' ,$message );
+                }else{
+                    return redirect($this->baseUrl)->with(['error_message' => __('webCaption.alert_somthing_wrong.title') ]);
+                }
+                
+            }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {   
+        if (!Auth::user()->can('common-ports-edit')) {
+            abort(403);
+        }
+        $data = Ports::select('id', 'name','title_languages', 'parent_id', 'display')->where('id', $id)->first();
+        $activeSiteLanguages = SiteLanguage::ActiveSiteLanguagesForMaster();
+        $breadcrumbs[0] = [
+            'link' => $this->baseUrl,
+            'name' => __('webCaption.list.title')
+        ];   
+
+        //send id as value because dynamic select work with  id as value  name as name  
+        $parent_data = Ports::select('id as value', 'name', 'parent_id', 'display')->orderBy('name', 'ASC')->where('parent_id', '0')->get();
+        return view('content.admin.masters.common.ports.create-form',['data' => $data,'breadcrumbs' =>$breadcrumbs ,'activeSiteLanguages' => $activeSiteLanguages ,'parent_data' => $parent_data ,'menuUrl' =>$this->menuUrl]);
+   
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+ 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request) {
+
+        if (!Auth::user()->can('common-ports-delete')) {
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_delete_access.title'); 
+            return response()->json(['result' => $result]);
+            abort(403);
+        }
+
+        if(Ports::where('id', $request->id)->firstorfail()->delete()){
+            $result['status']     = true;
+            $result['message']    = __('webCaption.alert_deleted_successfully.title'); 
+        
+           return response()->json(['result' => $result]);
+
+        }else{
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_somthing_wrong.title'); 
+            return response()->json(['result' => $result]);
+        }
+    }
+
+    public function deleteMultiple( Request $request){
+
+        if (!Auth::user()->can('common-ports-delete')) {
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_delete_access.title'); 
+            return response()->json(['result' => $result]);
+            abort(403);
+        }
+
+        if(Ports::whereIn('id', $request->delete_ids)->delete()){
+            $result['status']     = true;
+            $result['message']    = __('webCaption.alert_deleted_successfully.title') ;
+           return response()->json(['result' => $result]);
+
+        }else{
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_somthing_wrong.title'); 
+            return response()->json(['result' => $result]);
+        }
+
+        
+    }
+
+
+    public function updateStatus(Request $request){
+
+        
+        if (!Auth::user()->can('common-ports-edit')) {
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_update_access.title'); 
+            return response()->json(['result' => $result]);
+            abort(403);
+        }
+
+        $__data = Ports::FindOrFail($request->id);
+
+        if(isset($__data->display)){
+            $display =  ($__data->display == "Yes")? "No" : "Yes";
+            // $__data->display = $display;
+            $__data->update(['display' => $display]);  
+            $result['status']     = true;
+            $result['message']    = __('webCaption.alert_updated_successfully.title'); 
+        }else{
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_somthing_wrong.title'); 
+
+        }
+
+
+        return response()->json(['result' => $result]);
+    }
+}
---
+++ laravel/resources/views/content/admin/masters/common/ports/list.blade.php
@@ -0,0 +1,158 @@
+@extends('layouts/contentLayoutMaster')
+@section('title', $pageConfigs['moduleName'])
+
+@section('content')
+<!-- users list start -->
+<section>
+  <div class="row">
+    <div class="col-12">
+            <!-- filter  -->
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title" data-toggle="tooltip" data-placement="right" title="{{__('webCaption.search_filter.caption')}}">{{__('webCaption.search_filter.title')}}</h4>                    
+        </div>
+        <div class="card-body">
+          <form method="GET" action="{{route('common.ports.index')}}">
+            <div class="row">
+                <div class="col-sm-4 col-md-5 col-lg-3 col-xl-4">
+                    <div class="form-group">
+                        <x-admin.form.inputs.text id="searchKeyword" for="{{__('webCaption.keyword.title')}}" label="{{__('webCaption.keyword.title')}}" tooltip="{{__('webCaption.keyword.caption')}}"  class="form-control" name="search[keyword]"  placeholder="{{__('webCaption.keyword.title')}}" value="{{ request()->input('search.keyword') }}"  required="" />
+                    </div>
+                </div>
+                <div class="col-7 col-sm-3 col-md-4 col-lg-3">
+                    <div class="form-group">
+                        <x-admin.form.label for="" value="{{__('webCaption.display_status.title')}}" class="" tooltip="{{__('webCaption.display_status.caption')}}" />
+                        <div>
+                                <div class="form-check form-check-inline">
+                                <x-admin.form.inputs.radio for="searchDisplayStatusOn" class="border border-danger" name="search[displayStatus]" tooltip="{{__('webCaption.yes.caption')}}" label="{{__('webCaption.yes.title')}}" value="Yes"  required=""  checked="{{ (request()->input('search.displayStatus') ) == 'Yes' ? 'checked' : '' }}" required="" />&ensp;
+                                    
+                                <x-admin.form.inputs.radio for="searchDisplayStatusOff" class="border border-danger" name="search[displayStatus]" label="{{__('webCaption.no.title')}}" tooltip="{{__('webCaption.no.caption')}}" value="No"  required=""  checked="{{ (request()->input('search.displayStatus') ) == 'No' ? 'checked' : '' }}" required="" />&ensp;
+
+                                <x-admin.form.inputs.radio for="searchDisplayStatusAll" class="border border-danger" name="search[displayStatus]" label="{{__('webCaption.all.title')}}" tooltip="{{__('webCaption.all.caption')}}" value=""  required=""  checked="{{  ( (request()->input('search.displayStatus') ) == null ) || ( (request()->input('search.displayStatus') ) == '' )  ? 'checked' : '' }}" required="" />&ensp;
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-5 col-sm-3 col-md-3 col-lg-2 col-xl-2">
+                    <div class="row">
+                        <div class="col-12 col-md-10">
+                            <div class="form-group">
+                                <x-admin.form.label for=""  value="{{__('webCaption.parent_only.title')}}" class="" tooltip="{{__('webCaption.parent_only.caption')}}" />
+
+                                <x-admin.form.inputs.checkbox for="searchParentOnlyShowAll"  name="search[parentOnlyShowAll]" tooltip="{{__('webCaption.show_all.caption')}}" label="{{__('webCaption.show_all.title')}}" checked="{{ (request()->input('search.parentOnlyShowAll') == 1)  ?'checked' :''; }}"  value="1"  customClass="form-check-input"  />
+                                
+                            </div>
+                        </div>
+                        
+                    </div> 
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-4 col-xl-3 pt-1 ">
+                    <x-admin.form.buttons.search />
+                    <x-admin.form.buttons.reset href="{{route('common.ports.index')}}" />
+                </div>
+            </div>
+          </form>
+        </div>
+      </div>    
+        @php
+        $request_params = request()->all();
+        unset( $request_params['order'], $request_params['order_by'] );
+        @endphp
+                    
+        <div class="card">
+        <!-- Basic Tables start -->
+          <div class="card-body">
+            @can('common-ports') 
+                @if(count($data) > 0 )
+                    <div class="table-responsive">
+                        <div class="mt-2">
+                            {{ $data->onEachSide(2)->appends(request()->query())->links('vendor.pagination.bootstrap-4') }}       
+                        </div>
+                        @can('common-ports-delete')
+                            <div class="px-2 my-2">
+                                {{-- deleteMultiple() for delete multiple data pass url here  --}}
+                                <x-admin.form.buttons.multipleDelete url="{{route('common.ports.delete-multiple')}}" />
+                            </div>
+                        @endcan
+                        <table class="table" id="master-list">
+                            <thead>
+                                <tr>
+                                        <th> <x-admin.form.inputs.multiple_select_checkbox id="checkAll"   value="1"  customClass=""  /> </th>
+                                        <th class="position-for-filter-heading"># <x-admin.filter.order-by-filter-div orderBy="id" />
+                                        </th>                                                
+                                        <th class="position-for-filter-heading" data-toggle="tooltip" title="{{__('webCaption.ports.caption')}}"> {{__('webCaption.ports.title')}}<x-admin.filter.order-by-filter-div orderBy="name" />
+                                        </th>
+                                        <th class="position-for-filter-heading" data-toggle="tooltip" title="{{__('webCaption.no_of_children.caption')}}" >{{__('webCaption.no_of_children.title')}}<x-admin.filter.order-by-filter-div orderBy="children_count" />
+                                        </th>
+                                        <th class="position-for-filter-heading" data-toggle="tooltip" title="{{__('webCaption.display_status.caption')}}"  >
+                                            {{__('webCaption.display_status.title')}} <x-admin.filter.order-by-filter-div orderBy="display" />
+                                        </th>
+                                        <th data-toggle="tooltip" title="{{__('webCaption.actions.caption')}}" >{{__('webCaption.actions.title')}}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($data as $item)
+                                    @include('content.admin.masters.common.ports.item-tr', ['item'=>$item])    
+                                    @if( true || request()->input('search.parentOnlyShowAll') == 1)
+                                        @foreach($item->children as $childItem)
+                                            @include('content.admin.masters.common.ports.item-tr', ['item'=>$childItem])    
+                                        @endforeach 
+                                    @endif
+                                @endforeach   
+                                            
+                            </tbody>                            
+                        </table>
+                        <div class="mt-2">
+                            {{ $data->onEachSide(2)->appends(request()->query())->links('vendor.pagination.bootstrap-4') }}       
+                        </div>
+                    </div>
+                @else
+                    <div class="text-center my-2">
+                        <h3>{{__('webCaption.record_not_found.title')}} </h3>
+                    </div>
+                @endif    
+            @endcan
+            </div>
+        </div>    
+    </div>
+  </div>     
+</div>
+   
+<!-- list section end -->
+</section>
+
+{{-- this file include for delete alert box  --}}
+@include('components.admin.alerts.delete-alert-box')
+@include('components.admin.alerts.multiple-delete-alert-box')
+@include('components.admin.filter.order-by')
+<!-- users list ends -->
+
+@endsection
+
+@section('page-script')
+
+{{--  --}}
+<script type="text/javascript">
+$('.load-child-records').click( function(event){
+    event.preventDefault();
+    var eObject = this;
+    var itemId = $(this).attr('data-itemId');
+    var parent_tr = $(this).closest('tr');
+
+    if( $(this).hasClass('collasped') ) {
+        $(eObject).removeClass('collasped').addClass('expanded');
+        $(eObject).find('i:first').removeClass('fa-caret-right').addClass('fa-caret-down');
+        $('.parent-id-' + itemId).show();
+    } else {
+        $(eObject).removeClass('expanded').addClass('collasped');
+        $(eObject).find('i:first').removeClass('fa-caret-down').addClass('fa-caret-right');
+        $('.parent-id-' + itemId).hide();
+    }
+});
+</script>
+
+@endsection
+
+
+ 
+
