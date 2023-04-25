3786ab18cb033e4ece923730d5bd864ec843d958
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-25 11:33:41 +0530

    remove list loop

--- laravel/resources/views/content/admin/masters/vechiles/makes/list-loop.blade.php
+++
@@ -1,4 +0,0 @@
-@foreach($data as $item)
-    @include('content.admin.masters.vechiles.makes.item-tr', ['item'=>$item])    
-
-@endforeach  
--- laravel/resources/views/content/admin/masters/vechiles/types/list-loop.blade.php
+++
@@ -1,4 +0,0 @@
-@foreach($data as $item)
-    @include('content.admin.masters.vechiles.types.item-tr', ['item'=>$item])    
-    
-@endforeach  
