8a357a67015705d3832799b83fac7e3f17bccd47
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-25 12:02:38 +0530

    region view added

--- laravel/resources/views/content/admin/masters/common/region/item-tr.blade.php
+++ laravel/resources/views/content/admin/masters/common/region/item-tr.blade.php
@@ -1,25 +1,63 @@
-<tr>
-    <td>
-        <x-admin.form.inputs.multiple_select_checkbox id="select{{$item->id}}"   value="{{$item->id}}"  customClass="checkbox"  />            
-    </td>
-    <td>{{$item->id}}</td>
+    @php
+        
+         $display = ( ($item->parent_id != null) && request()->input('search.parentOnlyShowAll') != 1  ) ? "item-tr-display-none" :'';
+         $childTdColor = ( $item->parent_id > 0 )? "child-td-color" :'';
 
-    <td>
-        @php echo  str_ireplace( request()->input('search.keyword'), '<span class="heighlight-string" >'. request()->input('search.keyword').'</span>',$item->name) @endphp
-    </td>
-    <td>
-        @if(isset($item->state->name)) {{$item->state->name}} @endif
-    </td>  
+         $marginLeft = '0';
+            if( $item->parent_id > 0 ) {
+                $marginLeft = "1.5rem;";
+            }
+    @endphp
 
-    <td>
-    @can('main-navigation-common-region-edit')
-    <x-admin.form.buttons.edit href="{{ route('common.region.edit', $item->id) }}" />&ensp;
-    @endcan
-    &nbsp;
+    <tr class="parent-id-{{$item->parent_id}} {{$display}}">
+        <td>
+            <x-admin.form.inputs.multiple_select_checkbox id="select{{$item->id}}"   value="{{$item->id}}"  customClass="checkbox"  />            
+        </td>
+        <td @if($childTdColor != '') class="{{$childTdColor}}" @endif >
+            <span style=" margin-left: {{$marginLeft}}">{{$item->id}}</span>
+        </td>
+        <td @if($childTdColor != '') class="{{$childTdColor}}" @endif>
+             @php echo  str_ireplace( request()->input('search.keyword'), '<span class="heighlight-string" >'. request()->input('search.keyword').'</span>',$item->name) @endphp
+        </td>
+
+        <td>
+            @if( $item->children_count > 0 )
+                @php
+                    $collapsedClass = 'collasped';
+                    $caretClass = 'fa-caret-right';
+               
+                    if(request()->input('search.parentOnlyShowAll') == 1){
+                        $collapsedClass = 'expanded';
+                        $caretClass = 'fa-caret-down';
+                    }
+                @endphp
+
+                <a href="#" class="load-child-records {{$collapsedClass}}" data-itemId="{{$item->id}}">{{$item->children_count}} <i class="fa {{$caretClass}}"></i> </a>
+            @else
+                {{$item->children_count}}
+            @endif                                        
+        </td>
+        <td>
+            @php
+                $displayStatusChecked = '';
+                if( strcasecmp($item->display, 'Yes') == 0) {
+                    $displayStatusChecked = 'checked'; 
+                }
+            @endphp
     
-    @can('main-navigation-common-region-delete')
-    <x-admin.form.buttons.delete id="{{$item->id}}" name="{{$item->title}}" url="{{route('common.region.delete')}}" action="{{route('common.region.delete', $item->id) }}" />  
-    @endcan
+            <x-admin.form.inputs.listing_checkbox id="list{{$item->id}}"  onclick="changeDisplayStatus('{{$item->id}}','{{route('common.region.update-status')}}')"  dataItemId="{{$item->id}}" dataUrl="{{route('common.region.update-status')}}" 
+               value="{{$item->id}}" checked="{{$displayStatusChecked}}"  /> 
+        </td>
+        <td>
+            @can('common-region-edit')
+             <x-admin.form.buttons.edit href="{{ route('common.region.edit', $item->id) }}" />
+            @endcan
+            &nbsp;
 
-    </td>
-</tr>
+           {{-- pass in  deleteSingleData(id , name ,url ) for delete  --}}
+
+           @can('common-region-delete')
+            <x-admin.form.buttons.delete id="{{$item->id}}" name="{{$item->name}}" url="{{route('common.region.delete')}}" action="{{route('common.region.delete',$item->id)}}" /> 
+           @endcan
+        </td>
+    </tr>
