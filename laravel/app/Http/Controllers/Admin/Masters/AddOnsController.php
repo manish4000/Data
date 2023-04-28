db47fd1233125a698ea6e114f9dd7848e7d3dc4e
manisheximgroup <manisheximgroup1@gmail.com>
2023-04-28 16:18:21 +0530

    add new module adds one

---
+++ laravel/app/Http/Controllers/Admin/Masters/AddOnsController.php
@@ -0,0 +1,282 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Masters;
+
+use App\Http\Controllers\Controller;
+use App\Models\Masters\AddOns;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\UrlGenerator;
+
+use App\Models\SiteLanguage;
+use Illuminate\Support\Facades\Auth;
+
+class AddOnsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    protected $baseUrl      =   '';
+    protected $url;
+    public $menuUrl ='admin/masters/billing/add-ons';
+
+
+    public function __construct(UrlGenerator $url) {
+        $this->url = $url;
+        $this->baseUrl =  $this->url->to('/admin/masters/billing/add-ons');
+    }
+
+    public function index(Request $request)
+    {    
+        if (!Auth::user()->can('masters-billing-add-ons')) {
+            abort(403);
+        }
+
+        $pageConfigs = [
+            'baseUrl' => $this->baseUrl, 
+            'moduleName' => __('webCaption.menu_masters_add_ons.title'), 
+        ];
+
+        if (Auth::user()->can('masters-billing-add-ons-add')) {
+            $breadcrumbs[0] = [
+                'link' => $this->baseUrl.'/add',
+                'name' => __('webCaption.add.title'), 
+            ];
+        }else{
+            $breadcrumbs[0] = [];
+        }
+        
+        $data = AddOns::withCount('children');
+
+        if(  $request->has('search.keyword')) {
+            $data->keywordFilter($request->input('search.keyword')); 
+        }
+        if(  $request->has('search.displayStatus') && !empty($request->input('search.displayStatus'))) {
+            $data->displayStatusFilter($request->input('search.displayStatus')); 
+        }   
+        
+       // if(  !$request->has('search.parentOnlyShowAll')) {
+            $data->parentOnlyFilter();
+        //}
+        if($request->has('order_by') &&  $request->has('order') ){
+            $data->orderBy($request->order_by, $request->order);
+        }
+
+        $data->with(['children'=>function($query) use ($request) {
+            if(  $request->has('search.keyword')) {
+                $query->keywordFilter($request->input('search.keyword')); 
+            }
+            if(  $request->has('search.displayStatus') && !empty($request->input('search.displayStatus'))) {
+                $query->displayStatusFilter($request->input('search.displayStatus')); 
+            }
+
+            if($request->has('order_by') != "children_count"){
+                
+                if($request->has('order_by') &&  $request->has('order') ){
+                    $query->orderBy($request->order_by, $request->order);
+                }
+            }
+
+        } ]); 
+
+        $perPage =  (isset($request->perPage) && !empty($request->perPage)) ? $request->perPage : 100;
+
+        $data = $data->paginate($perPage);
+
+        return view('content.admin.masters.billing.add_ons.list', ['pageConfigs' => $pageConfigs, 'breadcrumbs' => $breadcrumbs, 'data'=>$data,'perPage' => $perPage]);
+    }
+
+
+    //send id as value because dynamic select work with  id as value  name as name  
+
+    public function add() {
+
+        if (!Auth::user()->can('masters-billing-add-ons-add')) {
+            abort(403);
+        }
+        $parent_data = AddOns::select('id as value', 'name', 'parent_id', 'display')->orderBy('name', 'ASC')->where('parent_id', '0')->get();
+        $data = array();
+        $breadcrumbs[0] = [
+            'link' => $this->baseUrl,
+            'name' => __('webCaption.list.title')
+        ];
+        return view('content.admin.masters.billing.add_ons.create-form',['data' => $data ,'menuUrl' =>$this->menuUrl,'breadcrumbs' =>$breadcrumbs ,'parent_data' => $parent_data  ]);
+    }
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+         
+        if($request->id){
+            if (!Auth::user()->can('masters-billing-add-ons-edit')) {
+                abort(403);
+            }
+            $add_ons_model =   AddOns::find($request->id);
+        }else{
+            if (!Auth::user()->can('masters-billing-add-ons-add')) {
+                abort(403);
+            }
+            $add_ons_model =   new AddOns;
+        }
+
+        $validator = Validator::make($request->all(),
+          [
+            'display' => 'required',
+            'name' => 'required|unique:add_ons,name,'.$request->id.',id,deleted_at,NULL' 
+          ]  ,
+          [
+            'name.required' => __('webCaption.validation_required.title', ['field'=> __('webCaption.name.title')  ] ),
+            'display.required' => __('webCaption.validation_required.title', ['field'=> __('webCaption.display.title')  ] ),
+            'name.unique' => __('webCaption.validation_unique.title', ['field'=> $request->input('name')] ),
+          ]);
+    
+        if ($validator->fails()) {
+            return redirect()->back()->with('errors', $validator->errors() )->withInput();
+        }
+
+        
+
+                $add_ons_model->name       =   $request->name;
+                $add_ons_model->parent_id  =   isset($request->parent_id)? $request->parent_id : 0 ;
+                $add_ons_model->display    =   $request->display;
+  
+
+                if($add_ons_model->save()){
+                    $message = (isset($request->id)) ? $request->name." ". __('webCaption.alert_updated_successfully.title') : $request->name." ".__('webCaption.alert_added_successfully.title') ;
+                    return redirect()->route('masters.billing.add-ons.index')->with('success_message' ,$message );
+                }else{
+                    return redirect($this->baseUrl)->with(['error_message' => __('webCaption.alert_somthing_wrong.title') ]);
+                }
+                
+            }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {   
+        if (!Auth::user()->can('masters-billing-add-ons-edit')) {
+            abort(403);
+        }
+        $data = AddOns::select('id', 'name','title_languages', 'parent_id', 'display')->where('id', $id)->first();
+        $activeSiteLanguages = SiteLanguage::ActiveSiteLanguagesForMaster();
+        $breadcrumbs[0] = [
+            'link' => $this->baseUrl,
+            'name' => __('webCaption.list.title')
+        ];   
+
+        //send id as value because dynamic select work with  id as value  name as name  
+        $parent_data = AddOns::select('id as value', 'name', 'parent_id', 'display')->orderBy('name', 'ASC')->where('parent_id', '0')->get();
+        return view('content.admin.masters.billing.add_ons.create-form',['data' => $data,'breadcrumbs' =>$breadcrumbs ,'activeSiteLanguages' => $activeSiteLanguages ,'parent_data' => $parent_data ,'menuUrl' =>$this->menuUrl]);
+   
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+ 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request) {
+
+        if (!Auth::user()->can('masters-billing-add-ons-delete')) {
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_delete_access.title'); 
+            return response()->json(['result' => $result]);
+            abort(403);
+        }
+
+        if(AddOns::where('id', $request->id)->firstorfail()->delete()){
+            $result['status']     = true;
+            $result['message']    = __('webCaption.alert_deleted_successfully.title'); 
+        
+           return response()->json(['result' => $result]);
+
+        }else{
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_somthing_wrong.title'); 
+            return response()->json(['result' => $result]);
+        }
+    }
+
+    public function deleteMultiple( Request $request){
+
+        if (!Auth::user()->can('masters-billing-add-ons-delete')) {
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_delete_access.title'); 
+            return response()->json(['result' => $result]);
+            abort(403);
+        }
+
+        if(AddOns::whereIn('id', $request->delete_ids)->delete()){
+            $result['status']     = true;
+            $result['message']    = __('webCaption.alert_deleted_successfully.title') ;
+           return response()->json(['result' => $result]);
+
+        }else{
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_somthing_wrong.title'); 
+            return response()->json(['result' => $result]);
+        }
+
+        
+    }
+
+
+    public function updateStatus(Request $request){
+ 
+        if (!Auth::user()->can('masters-billing-add-ons-edit')) {
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_update_access.title'); 
+            return response()->json(['result' => $result]);
+            abort(403);
+        }
+
+        $__data = AddOns::FindOrFail($request->id);
+
+        if(isset($__data->display)){
+            $display =  ($__data->display == "Yes")? "No" : "Yes";
+            // $__data->display = $display;
+            $__data->update(['display' => $display]);  
+            $result['status']     = true;
+            $result['message']    = __('webCaption.alert_updated_successfully.title'); 
+        }else{
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_somthing_wrong.title'); 
+
+        }
+
+        return response()->json(['result' => $result]);
+    }
+}
---
+++ laravel/resources/views/content/admin/masters/billing/add_ons/create-form.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts/contentLayoutMaster',['activeUrl' => $menuUrl])
+@if(isset($data->id) && !empty($data->id))
+@section('title', __('webCaption.add_ons.title'). ' ' .__('webCaption.edit.title')  )
+@else
+@section('title', __('webCaption.add_ons.title').' '. __('webCaption.add.title') )
+@endif
+
+@section('content')
+<form action="{{ route('masters.billing.add-ons.store')}}" method="POST">
+@csrf
+<section >
+  <div class="card">
+    <div class="card-header">
+			<h4 class="card-title">
+			<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map font-medium-3 mr-1"><polygon points="1 6 1 22 8 18 16 22 23 18 23 2 16 6 8 2 1 6"></polygon><line x1="8" y1="2" x2="8" y2="18"></line><line x1="16" y1="6" x2="16" y2="22"></line>
+			</svg>
+			@if(isset($data->id) && !empty($data->id))
+			{{__('webCaption.add_ons.title'). ' ' .__('webCaption.edit.title')  }}
+			@else
+			{{  __('webCaption.add_ons.title').' '. __('webCaption.add.title')}}
+			@endif
+			</h4>  
+		</div>
+		<hr class="m-0 p-0">
+    <div class="card-body">
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <div class="form-group">
+            <x-admin.form.inputs.text tooltip="{{__('webCaption.add_ons.caption')}}" label="{{__('webCaption.add_ons.title')}}" maxlength="50" for="name" name="name"  placeholder="{{ __('webCaption.add_ons.title') }}" value="{{old('name', isset($data->name)?$data->name:'' )}}"  required="required" />
+          </div>    
+        </div>
+
+        
+        <div class="col-lg-4 col-md-6">
+          <div class="form-group">
+            <x-admin.form.inputs.select  tooltip="{{__('webCaption.select_parent.caption')}}"  label="{{__('webCaption.select_parent.title')}}"  id="" for="parent_id" name="parent_id" placeholder="{{__('webCaption.select_parent.title')}}"  required="" :optionData="$parent_data" editSelected="{{(isset($data->parent_id) && ($data->parent_id != null))?$data->parent_id :''; }}" />
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+            <div class="form-group">
+                <x-admin.form.label for="" value="{{__('webCaption.display.title')}}" class="" tooltip="{{__('webCaption.display.caption')}}" />
+                   <div>
+                      <div class="form-check form-check-inline">
+                        <x-admin.form.inputs.radio for="Yes" tooltip="{{__('webCaption.yes.caption')}}"  class="border border-danger" name="display" label="{{__('webCaption.yes.title')}}" value="Yes"  required="required" 
+                        checked="{{ (old('display') == 'Yes') || (!isset($data->id))  ? 'checked' : '' }} {{ isset($data->display) ? $data->display == 'Yes' ? 'checked=checked' :'' :'' }} " required="required" />&ensp;
+                          
+                        <x-admin.form.inputs.radio for="No" class="border border-danger" name="display" tooltip="{{__('webCaption.no.caption')}}" label="{{__('webCaption.no.title')}}" value="No"  required="required"  checked="{{ old('display') == 'No' ? 'checked' : '' }} {{ isset($data->display) ? $data->display == 'No' ? 'checked=checked' :'' :'' }} " required="required" />&ensp;
+
+                      </div>
+                    </div>
+            </div>
+        </div>
+      </div>       
+    </div>
+  </div>
+  
+    @if((isset($data->parent_id)) && ($data->parent_id == 0) )
+
+          @php
+           $activeSiteLanguages = (isset($activeSiteLanguages)) ? $activeSiteLanguages : null;
+          @endphp
+          {{-- //this is showing for data form master data translation  --}}
+          <x-admin.site-language :activeSiteLanguages="$activeSiteLanguages" :data="$data->title_languages" name="title_languages" readonly="readonly" />
+    @endif
+  <div class="text-center">
+    <input type="hidden" name="id" value="@if(isset($data->id) && !empty($data->id)){{$data->id}}@endif" />
+      @if(isset($data->id))   <x-admin.form.buttons.update />  @else    <x-admin.form.buttons.create />   @endif
+  </div>
+</section>
+</form>
+@endsection
+
