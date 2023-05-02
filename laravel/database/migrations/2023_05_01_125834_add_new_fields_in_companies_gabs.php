83024d29bbe0fb66844158e95de0a4fd234b779d
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-02 15:56:15 +0530

    update fields in company and company gabs

---
+++ laravel/database/migrations/2023_05_01_125834_add_new_fields_in_companies_gabs.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies_gabs', function (Blueprint $table) {
+            $table->string('country_name',100)->nullable();
+            $table->string('state_name',100)->nullable();
+            $table->string('city_name',100)->nullable();
+            $table->string('region_name',100)->nullable();
+            $table->string('package_name',100)->nullable();
+            $table->string('association_member_name',100)->nullable();
+            $table->string('marketing_status_name',100)->nullable();
+            $table->string('plan_name',100)->nullable();
+            $table->string('deals_in_name',100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies_gabs', function (Blueprint $table) {
+            //
+        });
+    }
+};
