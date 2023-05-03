2db0cfd27b8d72a8758a47f39e09041e39e7b4ce
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-03 09:45:28 +0530

    update multiple select blade componant

--- laravel/resources/views/components/dash/form/inputs/multiple_select.blade.php
+++ laravel/resources/views/components/dash/form/inputs/multiple_select.blade.php
@@ -19,9 +19,20 @@ $border ="";
     }
 @endphp
 
+
+
+
 <div class="form-group">
-    @if (isset($label)) <label for='{{ $for }}'>{{ $label }} </label> @endif 
-    <select  class="select2-multiple" style="{{$border}}" data-autofill="true" multiple data-is-parent="false"  name="{{ $name }}" id="{{ $for }}" {{ $required }}>
+
+    @if (isset($label))
+    <label 
+         @if (isset($tooltip)) title="{{$tooltip}}"  @endif
+         data-toggle="tooltip"    for='{{ $for }}'>{{ $label }}
+         @if(isset($required) && !empty($required)) <span class="text-danger" style="font-size: 14px;font-weight: bolder"> * </span>  @endif 
+    </label>
+    @endif
+
+    <select  class="select2" data-autofill="true" multiple data-is-parent="false"  name="{{ $name }}" id="{{ $for }}" {{ $required }}>
         <option value="">Select</option>
         @if(isset($optionData) && count($optionData)>0)
             @foreach($optionData as $option)
@@ -34,9 +45,63 @@ $border ="";
     </select>
 </div>
 
-@section('page-script')
+@push('script')
+    <script>
+        
+    $(document ).ready(function() {
+        $('.select2').select2();
+    });
+        
+    </script>
+@endpush
 
-<script>
-    $('.select2-multiple').select2();
-</script>
-@endsection
+
+
+
+
+
+{{-- <div class="form-group">
+    @if (isset($label))
+        <label 
+             @if (isset($tooltip)) title="{{$tooltip}}"  @endif
+             data-toggle="tooltip"    for='{{ $for }}'>{{ $label }}
+             @if(isset($required) && !empty($required)) <span class="text-danger" style="font-size: 14px;font-weight: bolder"> * </span>  @endif 
+        </label>
+    @endif
+
+
+    <select  class="select2"    name="{{ $name }}" id="{{ $for }}" {{ $required }} multiple="multiple" >
+        @if(isset($optionData) && count($optionData) > 0)
+
+            @foreach($optionData as $option)
+            @php $sel = ''; @endphp
+
+                @if(is_array($oldValues) && in_array( $option->value, $oldValues)) @php $sel = 'selected="selected"'; @endphp 
+                @elseif( is_array($editSelected) && in_array($option->value, $editSelected)) @php $sel = 'selected="selected"'; @endphp @endif
+                <option value="{{$option->value}}" {{$sel}} >{{$option->name}}</option>
+            @endforeach
+        @endif
+    </select>
+</div>
+
+
+@if(isset($name)) 
+
+    <div class="m-0">
+        @if($errors->has($name))
+        <x-admin.form.form_error_messages message="{{ $errors->first($name) }}"  />
+        @endif
+    </div>
+
+@endif 
+
+
+@push('script')
+    <script>
+        
+    $(document ).ready(function() {
+        $('.select2').select2();
+    });
+        
+    </script>
+@endpush --}}
