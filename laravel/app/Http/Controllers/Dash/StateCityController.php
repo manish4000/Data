5a07330bd55464ba00fb543caf3b6bec336a1213
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-04 11:35:55 +0530

    add new method for country

--- laravel/app/Http/Controllers/Dash/StateCityController.php
+++ laravel/app/Http/Controllers/Dash/StateCityController.php
@@ -8,15 +8,22 @@ use Illuminate\Support\Facades\DB;
 
 class StateCityController extends Controller
 {
+
+    public  function countryList(Request $request){
+
+        $country  =   DB::table('countries')->where('country_id',$request->id)->orderBy('name')->get();
+        return response()->json(['countries' => $country]);
+    }
+
     public  function stateList(Request $request){
 
-        $states  =   DB::table('states')->where('country_id',$request->id)->get();
+        $states  =   DB::table('states')->where('country_id',$request->id)->orderBy('name')->get();
         return response()->json(['states' => $states]);
     }
 
     public  function cityList(Request $request){
 
-        $cities  =   DB::table('cities')->where('state_id',$request->id)->get();
+        $cities  =   DB::table('cities')->where('state_id',$request->id)->orderBy('name')->get();
 
         return response()->json(['cities' => $cities]);
 
