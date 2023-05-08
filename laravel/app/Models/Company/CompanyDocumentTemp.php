b8ab51a08e027596335a2b83081732aa2d13e27b
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-08 18:06:45 +0530

    new company docx temp added

---
+++ laravel/app/Models/Company/CompanyDocumentTemp.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Company;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyDocumentTemp extends Model
+{
+    use HasFactory;
+    protected $table = 'company_documents_temp';
+    protected $fillable = ['file_name','document_name'];
+}
