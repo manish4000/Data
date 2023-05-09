0e0eb993aa99c3a194b6fca0655922d86d0c1e75
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-09 18:36:01 +0530

    check box update

--- laravel/resources/views/components/dash/form/inputs/checkbox.blade.php
+++ laravel/resources/views/components/dash/form/inputs/checkbox.blade.php
@@ -10,7 +10,7 @@
 
 
 <div class="form-check form-check-inline">
-    <input  type="checkbox" name='{{ $name }}' class="form-check-input" @if(isset($for)) id="{{ $for }}" @endif
+    <input  type="checkbox" name='{{ $name }}' class="form-check-input " @if(isset($for)) id="{{ $for }}" @endif
          @if(isset($value)) value="{{$value}}" @endif @if(isset($checked)) {{$checked}} @endif >
          @if(isset($label))  <label class="form-check-label  @if(isset($labelClass)) {{$labelClass}} @endif " @if(isset($for)) for="" @endif > {{ $label }} </label>   @endif 
 </div>
