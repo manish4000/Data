7822ada6b0c0d9422a37da5cf9af1f84a283ad26
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-10 18:35:59 +0530

    sub category module added

---
+++ laravel/app/Http/Controllers/Dash/Masters/SubCategoryController.php
@@ -0,0 +1,293 @@
+<?php
+
+namespace App\Http\Controllers\Dash\Masters;
+
+use App\Http\Controllers\Controller;
+use App\Models\Dash\Masters\SubCategory;
+use App\Models\Dash\Masters\MainCategory;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\UrlGenerator;
+
+use App\Models\SiteLanguage;
+use Illuminate\Support\Facades\Auth;
+
+class SubCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    protected $baseUrl      =   '';
+    protected $url;
+    public $menuUrl ='masters/spare-parts/sub-category';
+
+
+    public function __construct(UrlGenerator $url) {
+        $this->url = $url;
+        $this->baseUrl =  $this->url->to('masters/spare-parts/sub-category');
+    }
+
+    public function index(Request $request)
+    {    
+        if (!Auth::guard('dash')->user()->can('masters-spare-parts-sub-category')) {
+            abort(403);
+        }
+
+        $pageConfigs = [
+            'baseUrl' => $this->baseUrl, 
+            'moduleName' => __('webCaption.masters_spare_parts_sub_category.title'), 
+        ];
+
+        if (Auth::guard('dash')->user()->can('masters-spare-parts-sub-category-add')) {
+            $breadcrumbs[0] = [
+                'link' => $this->baseUrl.'/create',
+                'name' => __('webCaption.add.title'), 
+            ];
+        }else{
+            $breadcrumbs[0] = [];
+        }
+        
+        $data = SubCategory::withCount('children')->with('mainCategory');
+
+        if($request->has('main_category') && $request->input('main_category') != '') {
+            $data->MainCategory($request->input('main_category')); 
+        }
+
+        if(  $request->has('search.keyword')) {
+            $data->keywordFilter($request->input('search.keyword')); 
+        }
+        if(  $request->has('search.displayStatus') && !empty($request->input('search.displayStatus'))) {
+            $data->displayStatusFilter($request->input('search.displayStatus')); 
+        }   
+        
+       // if(  !$request->has('search.parentOnlyShowAll')) {
+            $data->parentOnlyFilter();
+        //}
+        if($request->has('order_by') &&  $request->has('order') ){
+            $data->orderBy($request->order_by, $request->order);
+        }
+
+        $data->with(['children'=>function($query) use ($request) {
+            if(  $request->has('search.keyword')) {
+                $query->keywordFilter($request->input('search.keyword')); 
+            }
+            if(  $request->has('search.displayStatus') && !empty($request->input('search.displayStatus'))) {
+                $query->displayStatusFilter($request->input('search.displayStatus')); 
+            }
+
+            if($request->has('order_by') != "children_count"){
+                
+                if($request->has('order_by') &&  $request->has('order') ){
+                    $query->orderBy($request->order_by, $request->order);
+                }
+            }
+
+        } ]); 
+
+        $main_category = MainCategory::select('id as value','name')->orderBy('name')->get();
+
+        $perPage =  (isset($request->perPage) && !empty($request->perPage)) ? $request->perPage : 100;
+
+        $data = $data->paginate($perPage);
+
+        return view('dash.content.masters.spare_parts.sub_category.list', ['pageConfigs' => $pageConfigs, 'breadcrumbs' => $breadcrumbs, 'data'=>$data,'perPage' => $perPage , 'main_category'=>$main_category]);
+    }
+
+
+    //send id as value because dynamic select work with  id as value  name as name  
+
+    public function create() {
+
+        if (!Auth::guard('dash')->user()->can('masters-spare-parts-sub-category-add')) {
+            abort(403);
+        }
+        $parent_data = SubCategory::select('id as value', 'name', 'parent_id', 'display')->orderBy('name', 'ASC')->where('parent_id', '0')->get();
+        $data = array();
+        $breadcrumbs[0] = [
+            'link' => $this->baseUrl,
+            'name' => __('webCaption.list.title')
+        ];
+
+        $main_category = MainCategory::select('id as value','name')->orderBy('name')->get();
+
+        return view('dash.content.masters.spare_parts.sub_category.create-form',['data' => $data ,'menuUrl' =>$this->menuUrl,'breadcrumbs' =>$breadcrumbs ,'parent_data' => $parent_data, 'main_category'=>$main_category  ]);
+    }
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+         
+        if($request->id){
+            if (!Auth::guard('dash')->user()->can('masters-spare-parts-sub-category-edit')) {
+                abort(403);
+            }
+            $sub_category_model =  SubCategory::find($request->id);
+        }else{
+            if (!Auth::guard('dash')->user()->can('masters-spare-parts-sub-category-add')) {
+                abort(403);
+            }
+            $sub_category_model =   new SubCategory;
+        }
+
+        $validator = Validator::make($request->all(),
+          [
+            'display' => 'required',
+            'name' => 'required|unique:sub_categories,name,'.$request->id.',id,deleted_at,NULL',
+          ]  ,
+          [
+            'name.required' => __('webCaption.validation_required.title', ['field'=> __('webCaption.name.title')  ] ),
+            'display.required' => __('webCaption.validation_required.title', ['field'=> __('webCaption.display.title')  ] ),
+            'name.unique' => __('webCaption.validation_unique.title', ['field'=> $request->input('name')] ),
+          ]);
+    
+        if ($validator->fails()) {
+            return redirect()->back()->with('errors', $validator->errors() )->withInput();
+        }
+
+                $sub_category_model->name       =   $request->name;
+                $sub_category_model->parent_id  =   isset($request->parent_id)? $request->parent_id : 0 ;
+                $sub_category_model->display    =   $request->display;
+                $sub_category_model->main_category = $request->main_category;
+                // $sub_category_model->title_languages    =   $request->title_languages;
+
+                if($sub_category_model->save()){
+                    $message = (isset($request->id)) ? $request->name." ". __('webCaption.alert_updated_successfully.title') : $request->name." ".__('webCaption.alert_added_successfully.title') ;
+                    return redirect()->route('dashmasters.spare-parts.sub-category.index')->with('success_message' ,$message );
+                }else{
+                    return redirect($this->baseUrl)->with(['error_message' => __('webCaption.alert_somthing_wrong.title') ]);
+                }
+                
+            }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {   
+        if (!Auth::guard('dash')->user()->can('masters-spare-parts-sub-category-edit')) {
+            abort(403);
+        }
+        $data = SubCategory::select('id', 'name','title_languages', 'parent_id', 'display','main_category')->where('id', $id)->first();
+        $activeSiteLanguages = SiteLanguage::ActiveSiteLanguagesForMaster();
+        $breadcrumbs[0] = [
+            'link' => $this->baseUrl,
+            'name' => __('webCaption.list.title')
+        ];   
+
+        $main_category = MainCategory::select('id as value','name')->orderBy('name')->get();
+
+        //send id as value because dynamic select work with  id as value  name as name  
+        $parent_data = SubCategory::select('id as value', 'name', 'parent_id', 'display')->orderBy('name', 'ASC')->where('parent_id', '0')->get();
+        return view('dash.content.masters.spare_parts.sub_category.create-form',['data' => $data,'breadcrumbs' =>$breadcrumbs ,'activeSiteLanguages' => $activeSiteLanguages ,'parent_data' => $parent_data ,'menuUrl' =>$this->menuUrl , 'main_category'=>$main_category]);
+   
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+ 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request) {
+
+        if (!Auth::guard('dash')->user()->can('masters-spare-parts-sub-category-delete')) {
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_delete_access.title'); 
+            return response()->json(['result' => $result]);
+            abort(403);
+        }
+
+        if(SubCategory::where('id', $request->id)->firstorfail()->delete()){
+            $result['status']     = true;
+            $result['message']    = __('webCaption.alert_deleted_successfully.title'); 
+        
+           return response()->json(['result' => $result]);
+
+        }else{
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_somthing_wrong.title'); 
+            return response()->json(['result' => $result]);
+        }
+    }
+
+    public function deleteMultiple( Request $request){
+
+        if (!Auth::guard('dash')->user()->can('masters-spare-parts-sub-category-delete')) {
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_delete_access.title'); 
+            return response()->json(['result' => $result]);
+            abort(403);
+        }
+
+        if(SubCategory::whereIn('id', $request->delete_ids)->delete()){
+            $result['status']     = true;
+            $result['message']    = __('webCaption.alert_deleted_successfully.title') ;
+           return response()->json(['result' => $result]);
+
+        }else{
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_somthing_wrong.title'); 
+            return response()->json(['result' => $result]);
+        }
+
+        
+    }
+
+
+    public function updateStatus(Request $request){
+ 
+        if (!Auth::guard('dash')->user()->can('masters-spare-parts-sub-category-edit')) {
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_update_access.title'); 
+            return response()->json(['result' => $result]);
+            abort(403);
+        }
+
+        $__data = SubCategory::FindOrFail($request->id);
+
+        if(isset($__data->display)){
+            $display =  ($__data->display == "Yes")? "No" : "Yes";
+            // $__data->display = $display;
+            $__data->update(['display' => $display]);  
+            $result['status']     = true;
+            $result['message']    = __('webCaption.alert_updated_successfully.title'); 
+        }else{
+            $result['status']     = false;
+            $result['message']    = __('webCaption.alert_somthing_wrong.title'); 
+
+        }
+
+        return response()->json(['result' => $result]);
+    }
+}
---
+++ laravel/resources/views/dash/content/masters/spare_parts/sub_category/list.blade.php
@@ -0,0 +1,208 @@
+@extends('dash/layouts/LayoutMaster')
+@section('title', $pageConfigs['moduleName'])
+
+@section('content')
+<!-- users list start -->
+<section>
+    <div class="row">
+        <div class="col-12">
+            <!-- filter  -->
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title" data-toggle="tooltip" data-placement="right"
+                        title="{{__('webCaption.search_filter.caption')}}">{{__('webCaption.search_filter.title')}}</h4>
+                </div>
+                <div class="card-body">
+                    <form method="GET" action="{{route('dashmasters.spare-parts.sub-category.index')}}">
+                        <div class="row">
+                            <div class="col-sm-3 col-md-5 col-lg-7 col-xl-7">
+                                <div class="form-group">
+                                    <x-dash.form.inputs.text id="searchKeyword" for="{{__('webCaption.keyword.title')}}"
+                                        label="{{__('webCaption.keyword.title')}}"
+                                        tooltip="{{__('webCaption.keyword.caption')}}" class="form-control"
+                                        name="search[keyword]" placeholder="{{__('webCaption.keyword.title')}}"
+                                        value="{{ request()->input('search.keyword') }}" required="" />
+                                </div>
+                            </div>
+                            <div class="col-7 col-sm-3 col-md-4 col-lg-3">
+                                <div class="form-group">
+                                    <x-dash.form.label for="" value="{{__('webCaption.display_status.title')}}" class=""
+                                        tooltip="{{__('webCaption.display_status.caption')}}" />
+                                    <div>
+                                        <div class="form-check form-check-inline">
+                                            <x-dash.form.inputs.radio for="searchDisplayStatusOn"
+                                                class="border border-danger" name="search[displayStatus]"
+                                                tooltip="{{__('webCaption.yes.caption')}}"
+                                                label="{{__('webCaption.yes.title')}}" value="Yes" required=""
+                                                checked="{{ (request()->input('search.displayStatus') ) == 'Yes' ? 'checked' : '' }}"
+                                                required="" />&ensp;
+
+                                            <x-dash.form.inputs.radio for="searchDisplayStatusOff"
+                                                class="border border-danger" name="search[displayStatus]"
+                                                label="{{__('webCaption.no.title')}}"
+                                                tooltip="{{__('webCaption.no.caption')}}" value="No" required=""
+                                                checked="{{ (request()->input('search.displayStatus') ) == 'No' ? 'checked' : '' }}"
+                                                required="" />&ensp;
+
+                                            <x-dash.form.inputs.radio for="searchDisplayStatusAll"
+                                                class="border border-danger" name="search[displayStatus]"
+                                                label="{{__('webCaption.all.title')}}"
+                                                tooltip="{{__('webCaption.all.caption')}}" value="" required=""
+                                                checked="{{  ( (request()->input('search.displayStatus') ) == null ) || ( (request()->input('search.displayStatus') ) == '' )  ? 'checked' : '' }}"
+                                                required="" />&ensp;
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-5 col-sm-3 col-md-3 col-lg-2 col-xl-2">
+                                <div class="row">
+                                    <div class="col-md-10">
+                                        <div class="form-group">
+                                            <x-dash.form.label for="" value="{{__('webCaption.parent_only.title')}}"
+                                                class="" tooltip="{{__('webCaption.parent_only.caption')}}" />
+
+                                            <x-dash.form.inputs.checkbox for="searchParentOnlyShowAll"
+                                                name="search[parentOnlyShowAll]"
+                                                tooltip="{{__('webCaption.show_all.caption')}}"
+                                                label="{{__('webCaption.show_all.title')}}"
+                                                checked="{{ (request()->input('search.parentOnlyShowAll') == 1)  ?'checked' :''; }}"
+                                                value="1" customClass="form-check-input" />
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6">
+                                <div class="form-group">
+                                    <x-dash.form.inputs.select tooltip="{{__('webCaption.main_category.caption')}}"
+                                        label="{{__('webCaption.main_category.title')}}" id="" for="main_category"
+                                        name="main_category" placeholder="{{__('webCaption.main_category.title')}}"
+                                        required="" :optionData="$main_category"
+                                        editSelected="{{ request()->input('main_category') }}" />
+                                </div>
+                            </div>
+                            <div class="col-md-12 pt-1 text-center">
+                                <x-dash.form.buttons.search />
+                                <x-dash.form.buttons.reset
+                                    href="{{route('dashmasters.spare-parts.sub-category.index')}}" />
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            @php
+            $request_params = request()->all();
+            unset( $request_params['order'], $request_params['order_by'] );
+            @endphp
+
+            <div class="card">
+                <!-- Basic Tables start -->
+                <div class="card-body">
+
+                    @if(count($data) > 0 )
+                    <div class="table-responsive">
+                        <div class="mt-2">
+                            {{ $data->onEachSide(1)->appends(request()->query())->links('vendor.pagination.bootstrap-4') }}
+                        </div>
+                        <div class="px-2 my-2">
+                            {{-- deleteMultiple() for delete multiple data pass url here  --}}
+                            <x-dash.form.buttons.multipleDelete
+                                url="{{route('dashmasters.spare-parts.sub-category.delete-multiple')}}" />
+                        </div>
+                        <table class="table" id="master-list">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <x-dash.form.inputs.multiple_select_checkbox id="checkAll" value="1"
+                                            customClass="" />
+                                    </th>
+                                    <th class="position-for-filter-heading">#
+                                        <x-dash.filter.order-by-filter-div orderBy="id" />
+                                    </th>
+                                    <th class="position-for-filter-heading" data-toggle="tooltip"
+                                        title="{{__('webCaption.sub_category.caption')}}">
+                                        {{__('webCaption.sub_category.title')}}
+                                        <x-dash.filter.order-by-filter-div orderBy="name" />
+                                    </th>
+                                    <th class="position-for-filter-heading" data-toggle="tooltip"
+                                        title="{{__('webCaption.main_category.caption')}}">
+                                        {{__('webCaption.main_category.title')}}
+                                        <x-dash.filter.order-by-filter-div orderBy="main_category" />
+                                    </th>
+                                    <th class="position-for-filter-heading" data-toggle="tooltip"
+                                        title="{{__('webCaption.no_of_children.caption')}}">
+                                        {{__('webCaption.no_of_children.title')}}
+                                        <x-dash.filter.order-by-filter-div orderBy="children_count" />
+                                    </th>
+                                    <th class="position-for-filter-heading" data-toggle="tooltip"
+                                        title="{{__('webCaption.display_status.caption')}}">
+                                        {{__('webCaption.display_status.title')}}
+                                        <x-dash.filter.order-by-filter-div orderBy="display" />
+                                    </th>
+                                    <th data-toggle="tooltip" title="{{__('webCaption.actions.caption')}}">
+                                        {{__('webCaption.actions.title')}}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($data as $item)
+                                @include('dash.content.masters.spare_parts.sub_category.item-tr', ['item'=>$item])
+                                @if( true || request()->input('search.parentOnlyShowAll') == 1)
+                                @foreach($item->children as $childItem)
+                                @include('dash.content.masters.spare_parts.sub_category.item-tr', ['item'=>$childItem])
+                                @endforeach
+                                @endif
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                        <div class="mt-2">
+                            {{ $data->onEachSide(1)->appends(request()->query())->links('vendor.pagination.bootstrap-4') }}
+                        </div>
+                    </div>
+                    @else
+                    <div class="text-center my-2">
+                        <h3>{{__('webCaption.record_not_found.title')}} </h3>
+                    </div>
+                    @endif
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <!-- list section end -->
+</section>
+
+{{-- this file include for delete alert box  --}}
+@include('components.dash.alerts.delete-alert-box')
+@include('components.dash.alerts.multiple-delete-alert-box')
+@include('components.dash.filter.order-by')
+<!-- users list ends -->
+
+@endsection
+
+@section('page-script')
+
+{{--  --}}
+<script type="text/javascript">
+$('.load-child-records').click(function(event) {
+    event.preventDefault();
+    var eObject = this;
+    var itemId = $(this).attr('data-itemId');
+    var parent_tr = $(this).closest('tr');
+
+    if ($(this).hasClass('collasped')) {
+        $(eObject).removeClass('collasped').addClass('expanded');
+        $(eObject).find('i:first').removeClass('fa-caret-right').addClass('fa-caret-down');
+        $('.parent-id-' + itemId).show();
+    } else {
+        $(eObject).removeClass('expanded').addClass('collasped');
+        $(eObject).find('i:first').removeClass('fa-caret-down').addClass('fa-caret-right');
+        $('.parent-id-' + itemId).hide();
+    }
+});
+</script>
+
+@endsection
