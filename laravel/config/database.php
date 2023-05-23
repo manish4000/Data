fdd522b789a7141eafb17f03a383c063e6e6b3e8
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-23 18:23:10 +0530

    new database added

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -62,6 +62,25 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'dash' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL_DASH'),
+            'host' => env('DB_HOST_DASH', '127.0.0.1'),
+            'port' => env('DB_PORT_DASH', '3306'),
+            'database' => env('DB_DATABASE_DASH', 'forge'),
+            'username' => env('DB_USERNAME_DASH', 'forge'),
+            'password' => env('DB_PASSWORD_DASH', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
