b12e9031459c5d3c2cd5b7e4682ee2823f5d76b3
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-25 18:31:22 +0530

    update order by filter div in dash

--- laravel/resources/views/components/dash/filter/order-by-filter-div.blade.php
+++ laravel/resources/views/components/dash/filter/order-by-filter-div.blade.php
@@ -8,23 +8,23 @@ $orderBy_request = isset(request()->order_by)? request()->order_by :'';
 
 switch ($order) {
     case 'asc':
-        $icon = "fa-solid fa-arrow-up";
+        $icon = "fa-solid fa-arrow-down-long";
         break;
     case 'desc':
-        $icon = "fa-solid fa-arrow-down";
+        $icon = "fa-solid fa-arrow-up-long";
         break;
 }
 
 if($orderBy_request == $orderBy ){
      $icon = $icon;
 }else{
-    $icon ="fa-solid fa-arrows-up-down";
+    $icon ="";
 }
 @endphp
 
 <div 
-    style="position: absolute;top: 1px;left: 3px;padding:10px;width:100%;height:100%"
-    class="short-by-filter"
+    
+    class="short-by-filter filter-short"
     @if(isset($orderBy)) data-orderBy="{{$orderBy}}"  @endif >
-       <i class="{{$icon}}"></i> 
+    <i class="{{$icon}}"></i> 
 </div>
