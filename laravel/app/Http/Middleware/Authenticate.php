adf0647d8c17c90ef2093ed422b73719357b6871
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-26 14:16:06 +0530

    check

--- laravel/app/Http/Middleware/Authenticate.php
+++ laravel/app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        dd(Auth::user());
+        dd("sdsd");
 
         if (! $request->expectsJson()) {
 
