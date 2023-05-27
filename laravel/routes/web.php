681523ebf21b8f7b9d5c92725ce48c072db8bcd7
manisheximgroup <manisheximgroup1@gmail.com>
2023-05-27 12:22:29 +0530

    web and kernal update

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -71,10 +71,7 @@ use Illuminate\Support\Facades\Auth;
 
 // Auth::routes();
 
-//Auth::routes();
-Route::post('login',function(){
-    echo "post login called";die();
-})->name('login');
+Auth::routes();
 
 Route::get('google-v3-recaptcha', [GoogleV3CaptchaController::class, 'index']);
 Route::post('validate-g-recaptcha', [GoogleV3CaptchaController::class, 'validateGCaptch']);
